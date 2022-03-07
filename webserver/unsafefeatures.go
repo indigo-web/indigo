dfc79485b2fa6faeaf79494b2cb789159ced6891
fakefloordiv <real.floordiv@gmail.com>
2022-03-07 17:31:39 +0200

    unsafefeatures: currently there are only s2b & b2s from fasthttp, but the list of unsafe functions may be extended in future

---
+++ webserver/unsafefeatures.go
@@ -0,0 +1,28 @@
+package webserver
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+/*
+https://github.com/valyala/fasthttp#tricks-with-byte-buffers
+*/
+func s2b(s string) (b []byte) {
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	/* #nosec G103 */
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+
+	return b
+}
+
+/*
+same as s2b, but does the opposite, also described in link above
+*/
+func b2s(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
