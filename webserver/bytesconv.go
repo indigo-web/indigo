f4df506d37176de9bfa417e66834a76605ea576f
fakefloordiv <real.floordiv@gmail.com>
2022-03-11 02:03:38 +0200

    currently contains only own implementation of lowercasing an array of bytes. Works only for ascii, and uses a byte-trick, so that's why it's faster than std realization

---
+++ webserver/bytesconv.go
@@ -0,0 +1,11 @@
+package webserver
+
+/*
+toLowercase does stuff directly on the array, without allocating a new buffer,
+so original buffer will be affected
+*/
+func toLowercase(data []byte) {
+	for i, char := range data {
+		data[i] = char | 0x20
+	}
+}
