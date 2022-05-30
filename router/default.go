274d859217cef26a46c09ac7a4026089d7cec773
a very fake floordiv <real.floordiv@gmail.com>
2022-05-31 01:02:26 +0200

    renamed dispatcher to router, and changed interface. Now it's a bit different (including methods naming and one more method)

---
+++ router/default.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"indigo/types"
+)
+
+type ResponseWriter func(b []byte) error
+
+/*
+Router is an interface for every router may be used for indigo
+Actually, it's incompatible with http.Handler, so in case
+once I'll need to connect something like chi, I'll have to write
+a port for it. But looks like there won't be serious problems with that
+
+In case error is returned from OnRequest, OnError will be called with
+error was returned from OnRequest. So no need to call it internally
+manually, just trust me
+*/
+type Router interface {
+	OnRequest(request *types.Request, responseWriter ResponseWriter) error
+	OnError(err error)
+}
