27097ba2d5ef4d50299780a58ec3bfb41d44f2c1
a very fake floordiv <real.floordiv@gmail.com>
2022-06-03 18:58:07 +0200

    added more tests
    made the only test was before not that hardcoded, but using both pipe.Write() and pipe.Read() (was using own Read() implementation, idk for what), also added test cases when we instantly have an error, and after some writing (tests are now twice more lines of code than actual implementation)

--- tests/pipe_test.go
+++ tests/pipe_test.go
@@ -2,40 +2,75 @@ package tests
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"indigo/internal"
 )
 
-func readPipe(pipe *internal.Pipe, reschan chan []byte, errchan chan error) {
-	data, err := pipe.Read()
-
-	if err != nil {
-		errchan <- err
-		return
-	}
-
-	reschan <- data
-}
-
-func TestPipe(t *testing.T) {
+func TestPipeReadWrite(t *testing.T) {
 	wantedData := []byte("Hello!")
 
 	pipe := internal.NewPipe()
-	reschan, errchan := make(chan []byte), make(chan error)
-	go readPipe(pipe, reschan, errchan)
+
 	go func() {
 		pipe.Write(wantedData)
 	}()
 
-	select {
-	case gotData := <-reschan:
-		if !bytes.Equal(gotData, wantedData) {
-			t.Errorf("got invalid data from pipe (wanted %s, got %s)", string(wantedData), string(gotData))
-		}
-		return
-	case err := <-errchan:
+	data, err := pipe.Read()
+
+	if err != nil {
 		t.Errorf("got error: %s", err.Error())
 		return
+	}
+
+	if !bytes.Equal(data, wantedData) {
+		t.Errorf("got invalid data from pipe (wanted %s, got %s)", string(wantedData), string(data))
+	}
+}
+
+func TestPipeWriteErr(t *testing.T) {
+	wantedErr := io.EOF
+
+	pipe := internal.NewPipe()
+
+	go func() {
+		pipe.WriteErr(wantedErr)
+	}()
+
+	_, err := pipe.Read()
+
+	if err != wantedErr {
+		t.Fatalf("wanted: %s, got: %s", wantedErr.Error(), err.Error())
+	}
+}
+
+func TestPipeErrAfterWrite(t *testing.T) {
+	wantedData := []byte("Hello!")
+	wantedErr := io.EOF
+
+	pipe := internal.NewPipe()
+
+	go func() {
+		pipe.Write(wantedData)
+		pipe.WriteErr(wantedErr)
+	}()
+
+	data, err := pipe.Read()
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+		return
+	}
+
+	if !bytes.Equal(data, wantedData) {
+		t.Fatalf("data wanted: %s, got: %s", string(wantedData), string(data))
+		return
+	}
+
+	data, err = pipe.Read()
+
+	if err != wantedErr {
+		t.Fatalf("wanted error: %s, got: %s", wantedErr.Error(), err.Error())
 	}
 }
