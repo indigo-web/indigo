b414b90c3cf19f27fe09402d50f3d107b57081b8
a very fake floordiv <real.floordiv@gmail.com>
2022-07-01 01:08:34 +0200

    creations
    same as protos.go, but for methods. Just a set of enums of methods and func to get enum of byte-slice

---
+++ http/methods.go
@@ -0,0 +1,42 @@
+package http
+
+import "indigo/internal"
+
+type Method uint8
+
+const (
+	GET Method = iota + 1
+	HEAD
+	POST
+	PUT
+	DELETE
+	CONNECT
+	OPTIONS
+	TRACE
+	PATCH
+)
+
+func GetMethod(method []byte) Method {
+	switch internal.B2S(method) {
+	case "GET":
+		return GET
+	case "HEAD":
+		return HEAD
+	case "POST":
+		return POST
+	case "PUT":
+		return PUT
+	case "DELETE":
+		return DELETE
+	case "CONNECT":
+		return CONNECT
+	case "OPTIONS":
+		return OPTIONS
+	case "TRACE":
+		return TRACE
+	case "PATCH":
+		return PATCH
+	}
+
+	return 0
+}
