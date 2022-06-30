643d08ab2a5e5139416d4320fd3b6c75e67b5be4
a very fake floordiv <real.floordiv@gmail.com>
2022-07-01 00:56:47 +0200

    additions & renamings
    renamed HTTP* to ProtoHTTP* because of shouting linter VARIABLE STARTS WITH PACKAGE NAME. Fuck that linter. Also added function `GetProtocol` that converts protocol from byte-slice to enum

--- http/protos.go
+++ http/protos.go
@@ -1,11 +1,29 @@
 package http
 
+import "indigo/internal"
+
 type ProtocolVersion uint8
 
 const (
-	HTTP09 ProtocolVersion = iota + 1
-	HTTP10
-	HTTP11
+	ProtoHTTP09 ProtocolVersion = iota + 1
+	ProtoHTTP10
+	ProtoHTTP11
 
 	// HTTP2, HTTP3 won't be added until they won't be supported
 )
+
+func GetProtocol(proto []byte) ProtocolVersion {
+	// yes, dirty. Yes, it's bad. But it's fast!
+	internal.ToLowercase(proto)
+
+	switch internal.B2S(proto) {
+	case "http/0.9":
+		return ProtoHTTP09
+	case "http/1.0":
+		return ProtoHTTP10
+	case "http/1.1":
+		return ProtoHTTP11
+	}
+
+	return 0
+}
