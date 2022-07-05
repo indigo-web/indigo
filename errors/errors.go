f01c318ef86425e526c3dca744dac0e98eafe04f
a very fake floordiv <real.floordiv@gmail.com>
2022-07-05 14:19:54 +0200

    updates
    removed comment describing point of these errors cause of fucking linter

--- errors/errors.go
+++ errors/errors.go
@@ -4,7 +4,6 @@ import "errors"
 
 type Error error
 
-// user errors, e.g. getting a header that is not presented in the request
 var (
 	ErrDuplicatedHeader = errors.New("found a duplicated header")
 )
