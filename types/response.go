49d8738d171e05ed546fbfa8376c10a8a7271dc6
a very fake floordiv <real.floordiv@gmail.com>
2022-07-05 14:24:05 +0200

    improvements
    now response is that response that is expected to be

--- types/response.go
+++ types/response.go
@@ -1,10 +1,32 @@
 package types
 
+import (
+	"indigo/http"
+	"indigo/internal"
+)
+
 type Response struct {
-	code int16
+	Code    http.StatusCode
+	Headers http.Headers
+	Body    []byte
 }
 
-func (r *Response) WithCode(code int16) *Response {
-	r.code = code
+func (r *Response) WithCode(code http.StatusCode) *Response {
+	r.Code = code
+	return r
+}
+
+func (r *Response) WithHeader(key, value string) *Response {
+	r.Headers[key] = internal.S2B(value)
+	return r
+}
+
+func (r *Response) WithBody(body []byte) *Response {
+	r.Body = body
+	return r
+}
+
+func (r *Response) WithBodyString(body string) *Response {
+	r.Body = internal.S2B(body)
 	return r
 }
