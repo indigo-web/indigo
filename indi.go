da9c98376cf6f066440d054c528902e5f4179a5f
a very fake floordiv <real.floordiv@gmail.com>
2022-07-06 23:34:05 +0200

    updates
    removed all the testing stuff, now it is just a file with Application struct (and all the other clue for my self-made DI)

--- indi.go
+++ indi.go
@@ -1,67 +1,42 @@
-package main
+package indigo
 
 import (
 	"fmt"
 	"indigo/httpparser"
 	"indigo/httpserver"
+	"indigo/router"
 	"indigo/types"
 	"net"
 )
 
-type parser struct {
-	datas int
+// Application TODO: add domain to allow host multiple
+//                   domains in a single web-server instance
+type Application struct {
+	addr string
+	port uint16
 }
 
-func (p *parser) Parse(requestStruct *types.Request, data []byte) (done bool, extra []byte, err error) {
-	fmt.Println("[PARSER] having data:", string(data))
-	p.datas++
-
-	if p.datas%2 == 0 {
-		fmt.Println("[PARSER] request is done (returning something back)")
-		return true, data[len(data)/2:], nil
+func NewApp(addr string, port uint16) *Application {
+	return &Application{
+		addr: addr,
+		port: port,
 	}
-
-	return false, nil, nil
 }
 
-type router struct {
-}
-
-func (r router) OnRequest(request *types.Request, writeResponse types.ResponseWriter) (err error) {
-	fmt.Printf("[ROUTER-OnRequest] request=%v, writeResponse=%p\n", request, writeResponse)
-	fmt.Printf("[ROUTER-OnRequest] method=%d path=%s proto=%d headers=%s Easter=%s\n", request.Method, string(request.Path),
-		request.Protocol, request.Headers, string(request.Headers["Easter"]))
-
-	fmt.Println("[ROUTER-OnRequest] Writing response: Hello, world!")
-	if err = writeResponse([]byte("Hello, world!")); err != nil {
-		fmt.Println("[ROUTER-OnRequest] Failed to write a response:", err)
+func (a Application) Serve(router router.Router) error {
+	address := fmt.Sprintf("%s:%d", a.addr, a.port)
+	sock, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
 	}
-
-	return err
-}
-
-func (r router) OnError(err error) {
-	fmt.Println("[ROUTER-OnError] got error:", err)
-}
-
-func main() {
-	sock, err := net.Listen("tcp", "localhost:5000")
 	defer sock.Close()
 
-	if err != nil {
-		panic(err)
-	}
-
-	myRouter := router{}
-
-	fmt.Println("Listening on localhost:5000")
-
-	exitCode := httpserver.StartTCPServer(sock, func(conn net.Conn) {
+	return httpserver.StartTCPServer(sock, func(conn net.Conn) {
 		request, writeBody := types.NewRequest(make([]byte, 10), make(map[string][]byte), nil)
 		parser := httpparser.NewHTTPParser(&request, writeBody, httpparser.Settings{})
 
 		handler := httpserver.NewHTTPHandler(httpserver.HTTPHandlerArgs{
-			Router:           myRouter,
+			Router:           router,
 			Request:          &request,
 			WriteRequestBody: writeBody,
 			Parser:           parser,
@@ -74,6 +49,4 @@ func main() {
 		go handler.Poll()
 		httpserver.DefaultConnHandler(conn, handler.OnData)
 	})
-
-	fmt.Println("exited with error", exitCode)
 }
