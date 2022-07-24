76290061e70f7009789169e402b12a4bbde36b71
a very fake floordiv <real.floordiv@gmail.com>
2022-07-24 21:43:51 +0200

    additions & improvements
    added new constructor for pipe - `NewChanSizedPipe`, that has settings to set a cache size of channels. Ordinary `NewPipe` now is just a predicate for `NewChanSizedPipe` with arguments 0,0. Added method `Readable()`, that returns whether there is something in channels to read (goroutine-safe, of course; using `atomic.AddInt32()` for this)

--- internal/pipe.go
+++ internal/pipe.go
@@ -1,30 +1,46 @@
 package internal
 
+import "sync/atomic"
+
 type Pipe struct {
 	data  chan []byte
 	error chan error
+
+	entries int32
 }
 
-func NewPipe() *Pipe {
+func NewChanSizedPipe(dataChanSize, errChanSize int) *Pipe {
 	return &Pipe{
-		data:  make(chan []byte),
-		error: make(chan error),
+		data:  make(chan []byte, dataChanSize),
+		error: make(chan error, errChanSize),
 	}
 }
 
+func NewPipe() *Pipe {
+	return NewChanSizedPipe(0, 0)
+}
+
 func (p *Pipe) Write(b []byte) {
+	atomic.AddInt32(&p.entries, 1)
 	p.data <- b
 }
 
 func (p *Pipe) WriteErr(err error) {
+	atomic.AddInt32(&p.entries, 1)
 	p.error <- err
 }
 
 func (p *Pipe) Read() (b []byte, err error) {
+	defer atomic.AddInt32(&p.entries, -1)
+
 	select {
 	case b = <-p.data:
 		return b, nil
 	case err = <-p.error:
 		return nil, err
 	}
+}
+
+func (p *Pipe) Readable() bool {
+	return p.entries > 0
 }
