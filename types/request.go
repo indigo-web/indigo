f8c1839a5442c8ad46a0c0354e5956fe3262d5be
a very fake floordiv <real.floordiv@gmail.com>
2022-07-24 21:40:07 +0200

    additions
    added method `Request.GetFullBody()` in case you don't want stream-based body processing. It just accumulates body in buffer with default size set by a constant (in future I hope this will be replaced with an entry in settings). Also for err channel in pipe using sized with size of 1 err chan (reason is explained in comment above)

--- types/request.go
+++ types/request.go
@@ -9,6 +9,9 @@ type (
 	Params map[string][]byte
 )
 
+// DefaultBodySize TODO: set this value in settings
+const DefaultBodySize = 1024
+
 type Request struct {
 	Method   http.Method
 	Path     []byte
@@ -19,8 +22,10 @@ type Request struct {
 	body requestBody
 }
 
-func NewRequest(pathBuffer []byte, headers http.Headers, params Params) (Request, func([]byte)) {
-	pipe := *internal.NewPipe()
+func NewRequest(pathBuffer []byte, headers http.Headers, params Params) (Request, internal.Pipe) {
+	// pipe is sized chan because parser can write an error even before
+	// handler will be called
+	pipe := *internal.NewChanSizedPipe(0, 1)
 
 	return Request{
 		Path:     pathBuffer,
@@ -30,9 +35,32 @@ func NewRequest(pathBuffer []byte, headers http.Headers, params Params) (Request
 		body: requestBody{
 			body: pipe,
 		},
-	}, pipe.Write
+	}, pipe
 }
 
 func (r *Request) GetBody(bodyCb onBodyCallback, completeCb onBodyCompleteCallback) error {
 	return r.body.Read(bodyCb, completeCb)
+}
+
+func (r *Request) GetFullBody() []byte {
+	buffer := make([]byte, 0, DefaultBodySize)
+
+	// currently we have no limit for max body size
+	// so no error is possible to occur
+	_ = r.GetBody(
+		func(b []byte) error {
+			buffer = append(buffer, b...)
+			return nil
+		},
+		func(err error) {
+			return
+		},
+	)
+
+	return buffer
+}
+
+func (r *Request) Reset() {
+	// TODO: headers must also be reset
+	r.body.Reset()
 }
