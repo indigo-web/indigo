45cb35fc6fa5fe901dc5068f6d3cc25f8692dfb2
a very fake floordiv <real.floordiv@gmail.com>
2022-07-24 13:10:14 +0200

    updates
    now Response object renders itself. Of course, it's method is private, but can be accessed by public function that is not related to Response struct. Also added `GrowHeaders` method to increase capacity of headers

--- types/response.go
+++ types/response.go
@@ -3,42 +3,74 @@ package types
 import (
 	"indigo/http"
 	"indigo/internal"
-	"strconv"
 )
 
-type ResponseStruct struct {
+var (
+	headerKeyValueSplitter = []byte(": ")
+	crlf                   = []byte("\r\n")
+)
+
+type Response struct {
 	Code    http.StatusCode
-	Headers http.Headers
+	Headers [][]byte
 	Body    []byte
 }
 
-func Response() *ResponseStruct {
-	return &ResponseStruct{
-		Headers: make(http.Headers, 5),
-	}
+func NewResponse() Response {
+	return Response{}
 }
 
-func (r *ResponseStruct) WithCode(code http.StatusCode) *ResponseStruct {
+func (r Response) WithCode(code http.StatusCode) Response {
 	r.Code = code
 	return r
 }
 
-func (r *ResponseStruct) WithHeader(key, value string) *ResponseStruct {
-	r.Headers[key] = internal.S2B(value)
+func (r Response) WithHeaderByte(key, value []byte) Response {
+	r.Headers = append(r.Headers, renderHeader(key, value))
 	return r
 }
 
-func (r *ResponseStruct) GetHeaders() http.Headers {
-	r.Headers["Content-Length"] = internal.S2B(strconv.Itoa(len(r.Body)))
-	return r.Headers
+func (r Response) WithHeader(key, value string) Response {
+	return r.WithHeaderByte(internal.S2B(key), internal.S2B(value))
 }
 
-func (r *ResponseStruct) WithBody(body []byte) *ResponseStruct {
-	r.Body = body
+func (r Response) WithBodyByte(body []byte) Response {
+	r.Body = append(r.Body, body...)
 	return r
 }
 
-func (r *ResponseStruct) WithBodyString(body string) *ResponseStruct {
-	r.Body = internal.S2B(body)
+func (r Response) WithBody(body string) Response {
+	return r.WithBodyByte(internal.S2B(body))
+}
+
+func (r Response) GrowHeaders(newsize int) Response {
+	if cap(r.Headers) < newsize {
+		r.Headers = append(make([][]byte, 0, newsize), r.Headers...)
+	}
+
 	return r
+}
+
+/*
+render function takes buffer (that already starts with protocol and space),
+rendering and writing response directly into the response writer
+*/
+func (r Response) render(buff []byte, writer ResponseWriter) (rendered []byte, modifiedBuff []byte) {
+	// append code and status to buff
+	buff = append(append(buff, http.GetByteCodeTrailingSpace(r.Code)...), http.GetStatusTrailingCRLF(r.Code)...)
+
+	for _, header := range r.Headers {
+		buff = append(append(buff, header...), crlf...)
+	}
+
+	return append(append(buff, crlf...), r.Body...), buff
+}
+
+func renderHeader(key, value []byte) []byte {
+	return append(append(key, headerKeyValueSplitter...), value...)
+}
+
+func WriteResponse(buff []byte, response Response, writer ResponseWriter) (error, []byte) {
+	rendered, modifiedBuff := response.render(buff, writer)
+	return writer(rendered), modifiedBuff
 }
