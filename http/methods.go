2ba083428f1a3c6c656284ef7cf5689b8d90876f
a very fake floordiv <real.floordiv@gmail.com>
2022-07-24 21:32:35 +0200

    additions
    added new method for converting method into the string

--- http/methods.go
+++ http/methods.go
@@ -40,3 +40,28 @@ func GetMethod(method []byte) Method {
 
 	return 0
 }
+
+func Method2String(method Method) string {
+	switch method {
+	case GET:
+		return "GET"
+	case HEAD:
+		return "HEAD"
+	case POST:
+		return "POST"
+	case PUT:
+		return "PUT"
+	case DELETE:
+		return "DELETE"
+	case CONNECT:
+		return "CONNECT"
+	case OPTIONS:
+		return "OPTIONS"
+	case TRACE:
+		return "TRACE"
+	case PATCH:
+		return "PATCH"
+	}
+
+	return "UNKNOWN"
+}
