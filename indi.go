0909c6e851fdce795c29d424717e7f5972223820
a very fake floordiv <real.floordiv@gmail.com>
2022-08-02 13:01:02 +0200

    updates & improvements
    added graceful shutdown (implemented by Application.Shutdown() method), separated Application.Run() on a few smaller functions for better testing

--- indi.go
+++ indi.go
@@ -1,14 +1,15 @@
 package indigo
 
 import (
-	"errors"
 	"fmt"
+	"indigo/errors"
 	"indigo/http/parser"
 	"indigo/http/server"
 	"indigo/router"
 	"indigo/settings"
 	"indigo/types"
 	"net"
+	"sync"
 )
 
 // Application TODO: add domain to allow host multiple
@@ -16,6 +17,8 @@ import (
 type Application struct {
 	host string
 	port uint16
+
+	shutdown chan bool
 }
 
 func NewApp(host string, port uint16) *Application {
@@ -26,15 +29,9 @@ func NewApp(host string, port uint16) *Application {
 }
 
 func (a Application) Serve(router router.Router, maybeSettings ...settings.Settings) error {
-	var serverSettings settings.Settings
-
-	switch len(maybeSettings) {
-	case 0:
-		serverSettings = settings.Default()
-	case 1:
-		serverSettings = settings.Prepare(maybeSettings[0])
-	default:
-		return errors.New("too much settings (one struct is expected)")
+	serverSettings, err := getSettings(maybeSettings)
+	if err != nil {
+		return err
 	}
 
 	address := fmt.Sprintf("%s:%d", a.host, a.port)
@@ -44,7 +41,21 @@ func (a Application) Serve(router router.Router, maybeSettings ...settings.Setti
 	}
 	defer sock.Close()
 
-	return server.StartTCPServer(sock, func(conn net.Conn) {
+	return runTCPServer(sock, router, serverSettings, a.shutdown)
+}
+
+// Shutdown is a graceful shutdown that waits until all the clients
+// will disconnect by their own, without forced disconnecting
+// Calling this method blocks until next client will be connected
+// (and disconnected instantly)
+func (a Application) Shutdown() {
+	a.shutdown <- true
+	<-a.shutdown
+}
+
+func runTCPServer(sock net.Listener, router router.Router,
+	serverSettings settings.Settings, shutdown chan bool) error {
+	return server.StartTCPServer(sock, func(wg *sync.WaitGroup, conn net.Conn) {
 		request, pipe := types.NewRequest(
 			// TODO: add all this shit to settings
 			make([]byte, 10), make(map[string][]byte), nil,
@@ -56,12 +67,23 @@ func (a Application) Serve(router router.Router, maybeSettings ...settings.Setti
 			Request: &request,
 			Parser:  httpParser,
 			RespWriter: func(b []byte) error {
-				_, err = conn.Write(b)
+				_, err := conn.Write(b)
 				return err
 			},
 		})
 
 		go handler.Poll()
-		server.DefaultConnHandler(conn, handler.OnData)
-	})
+		server.DefaultConnHandler(wg, conn, handler.OnData)
+	}, shutdown)
+}
+
+func getSettings(maybeSettings []settings.Settings) (settings.Settings, error) {
+	switch len(maybeSettings) {
+	case 0:
+		return settings.Default(), nil
+	case 1:
+		return settings.Prepare(maybeSettings[0]), nil
+	default:
+		return settings.Settings{}, errors.ErrTooMuchSettings
+	}
 }
