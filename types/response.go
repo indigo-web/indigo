99c5c99195460f311cd0d12458690293b40affb0
a very fake floordiv <real.floordiv@gmail.com>
2022-08-14 23:14:09 +0200

    creations & deletions
    created response object (from scratch again), removed response rendering away as DTO must not do it. Originally rendering method was here due to bad design

--- types/response.go
+++ types/response.go
@@ -1,33 +1,37 @@
 package types
 
 import (
-	"indigo/http"
+	"indigo/http/status"
 	"indigo/internal"
-	"strconv"
 )
 
-var (
-	headerKeyValueSplitter = []byte(": ")
-	crlf                   = []byte("\r\n")
-)
+type ResponseWriter func([]byte) error
+
+// WithResponse is just a nil-filled default pre-created response. Because
+// of clear methods, it is anyway copied every time it is used as constructor
+// so please, DO NOT modify fields of this variable
+var WithResponse = Response{
+	Code:   status.OK,
+	Status: status.Text(status.OK),
+}
 
 type Response struct {
-	Code    http.StatusCode
-	Headers [][]byte
+	Code   status.Code
+	Status status.Status
+	// headers due to possible side effects are decided to be private
+	// also uninitialized response must ALWAYS have this value as nil
+	headers ResponseHeaders
 	Body    []byte
 }
 
-func NewResponse() Response {
-	return Response{}
-}
-
-func (r Response) WithCode(code http.StatusCode) Response {
+func (r Response) WithCode(code status.Code) Response {
 	r.Code = code
+	r.Status = status.Text(code)
 	return r
 }
 
-func (r Response) WithHeaderByte(key, value []byte) Response {
-	r.Headers = append(r.Headers, renderHeader(key, value))
+func (r Response) WithStatus(status status.Status) Response {
+	r.Status = status
 	return r
 }
 
@@ -35,44 +39,21 @@ func (r Response) WithHeader(key, value string) Response {
 	return r.WithHeaderByte(internal.S2B(key), internal.S2B(value))
 }
 
+func (r Response) WithHeaderByte(key, value []byte) Response {
+	r.headers = r.headers.Append(key, value)
+	return r
+}
+
+func (r Response) WithBody(body string) Response {
+	r.Body = internal.S2B(body)
+	return r
+}
+
 func (r Response) WithBodyByte(body []byte) Response {
 	r.Body = body
 	return r
 }
 
-func (r Response) WithBody(body string) Response {
-	return r.WithBodyByte(internal.S2B(body))
-}
-
-func (r Response) GrowHeaders(newsize int) Response {
-	if cap(r.Headers) < newsize {
-		r.Headers = append(make([][]byte, 0, newsize), r.Headers...)
-	}
-
-	return r
-}
-
-/*
-render function takes buffer (that already starts with protocol and space),
-rendering and writing response directly into the response writer
-*/
-func (r Response) render(buff []byte) (response []byte) {
-	// append code and status to buff
-	buff = append(append(buff, http.GetByteCodeTrailingSpace(r.Code)...), http.GetStatusTrailingCRLF(r.Code)...)
-
-	for _, header := range r.Headers {
-		buff = append(append(buff, header...), crlf...)
-	}
-	buff = append(buff, "Content-Length: "+strconv.Itoa(len(r.Body))+"\r\n\r\n"...)
-
-	return append(buff, r.Body...)
-}
-
-func renderHeader(key, value []byte) []byte {
-	return append(append(key, headerKeyValueSplitter...), value...)
-}
-
-func WriteResponse(buff []byte, response Response, writer ResponseWriter) (error, []byte) {
-	rendered := response.render(buff)
-	return writer(rendered), rendered
+func (r Response) Headers() ResponseHeaders {
+	return r.headers
 }
