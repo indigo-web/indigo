a92e9c90b010a77fe6b267779dbb01afdf4b0f3e
a very fake floordiv <real.floordiv@gmail.com>
2022-08-15 00:53:05 +0200

    additions
    added one more error - ErrTooManyHeaders

--- errors/errors.go
+++ errors/errors.go
@@ -10,6 +10,7 @@ var (
 	ErrURLTooLong            = errors.New("request URI too long")
 	ErrURLDecoding           = errors.New("invalid url encoding")
 	ErrUnsupportedProtocol   = errors.New("protocol is not supported")
+	ErrTooManyHeaders        = errors.New("too much headers")
 
 	ErrCloseConnection = errors.New("internal error as a signal")
 )
