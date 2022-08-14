f1b5080921c4c20d61c48314948815b6f77a422a
a very fake floordiv <real.floordiv@gmail.com>
2022-08-15 00:55:07 +0200

    improvements
    added settings support

--- indi.go
+++ indi.go
@@ -1,10 +1,12 @@
 package indigo
 
 import (
+	"errors"
 	"indigo/http/headers"
 	"indigo/http/parser/http1"
 	"indigo/http/server"
 	"indigo/router"
+	settings2 "indigo/settings"
 	"indigo/types"
 	"net"
 )
@@ -22,29 +24,47 @@ func NewApp(addr string) Application {
 	}
 }
 
-func (a Application) Serve(router router.Router) error {
+func (a Application) Serve(router router.Router, someSettings ...settings2.Settings) error {
+	settings, err := getSettings(someSettings...)
+	if err != nil {
+		return err
+	}
+
 	sock, err := net.Listen("tcp", a.addr)
 	if err != nil {
 		return err
 	}
 
 	return server.StartTCPServer(sock, func(conn net.Conn) {
-		// TODO: add settings and add this value right there
-		headersManager := headers.NewManager(5)
+		headersManager := headers.NewManager(settings.HeadersNumber)
 		request, gateway := types.NewRequest(headersManager)
-		// TODO: add startLine and headerBuff initial size to settings
-		startLineBuff := make([]byte, 0, 1024)
-		headerBuff := make([]byte, 0, 100)
+
+		startLineBuff := make([]byte, 0, settings.URLBuffSize.Default)
+		headerBuff := make([]byte, 0, settings.HeaderKeyBuffSize.Default)
+
 		httpParser := http1.NewHTTPRequestsParser(
-			request, startLineBuff, headerBuff, gateway,
+			request, gateway, startLineBuff, headerBuff, settings,
 		)
+
 		httpServer := server.NewHTTPServer(request, func(b []byte) error {
 			_, err := conn.Write(b)
 			return err
 		}, router, httpParser)
+
 		go httpServer.Run()
 
-		readBuff := make([]byte, 1024)
+		readBuff := make([]byte, settings.SockReadBufferSize.Default)
 		server.DefaultConnHandler(conn, readBuff, httpServer.OnData)
 	})
+}
+
+func getSettings(settings ...settings2.Settings) (settings2.Settings, error) {
+	switch len(settings) {
+	case 0:
+		return settings2.Default(), nil
+	case 1:
+		return settings2.Fill(settings[0]), nil
+	default:
+		return settings2.Settings{}, errors.New("too much settings")
+	}
 }
