935eaca7baf78dd921cf1c00d6bce401a9f11a4e
a very fake floordiv <real.floordiv@gmail.com>
2022-08-15 23:14:06 +0200

    updates
    renamed ErrRequestEntityTooLarge -> ErrTooLarge

--- errors/errors.go
+++ errors/errors.go
@@ -5,12 +5,12 @@ import (
 )
 
 var (
-	ErrBadRequest            = errors.New("bad request")
-	ErrRequestEntityTooLarge = errors.New("request entity too large")
-	ErrURLTooLong            = errors.New("request URI too long")
-	ErrURLDecoding           = errors.New("invalid url encoding")
-	ErrUnsupportedProtocol   = errors.New("protocol is not supported")
-	ErrTooManyHeaders        = errors.New("too much headers")
+	ErrBadRequest          = errors.New("bad request")
+	ErrTooLarge            = errors.New("too large")
+	ErrURLTooLong          = errors.New("request URI too long")
+	ErrURLDecoding         = errors.New("invalid url encoding")
+	ErrUnsupportedProtocol = errors.New("protocol is not supported")
+	ErrTooManyHeaders      = errors.New("too much headers")
 
 	ErrCloseConnection = errors.New("internal error as a signal")
 )
