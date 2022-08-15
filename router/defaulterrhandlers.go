4f3774ce87d2e3065f0382fbbc4fa6fdd6987c48
a very fake floordiv <real.floordiv@gmail.com>
2022-08-15 23:18:12 +0200

    improvements
    added handler for 413 Request Entity Too Large, other minor updates. For example, instead of integer literals as keys for errHandlers map, using now constants from http/status package

--- router/defaulterrhandlers.go
+++ router/defaulterrhandlers.go
@@ -7,16 +7,20 @@ import (
 
 var (
 	defaultBadRequest = types.WithResponse.
-				WithCode(status.BadRequest).
-				WithBody(`<h1 align="center">400 Bad Request</h1>`)
+		WithCode(status.BadRequest).
+		WithBody(`<h1 align="center">400 Bad Request</h1>`)
 
 	defaultNotFound = types.WithResponse.
-			WithCode(status.NotFound).
-			WithBody(`<h1 align="center">404 Request Page Not Found</h1>`)
+		WithCode(status.NotFound).
+		WithBody(`<h1 align="center">404 Request Page Not Found</h1>`)
 
 	defaultMethodNotAllowed = types.WithResponse.
-				WithCode(status.MethodNotAllowed).
-				WithBody(`<h1 align="center">405 Method Not Allowed</h1>`)
+		WithCode(status.MethodNotAllowed).
+		WithBody(`<h1 align="center">405 Method Not Allowed</h1>`)
+
+	defaultRequestEntityTooLarge = types.WithResponse.
+		WithCode(status.RequestEntityTooLarge).
+		WithBody(`<h1 align="center">413 Request Entity Too Large</h1>`)
 )
 
 type (
@@ -26,10 +30,11 @@ type (
 
 func newErrHandlers() errHandlers {
 	return errHandlers{
-		400: defaultBadRequestHandler,
-		404: defaultNotFoundHandler,
-		405: defaultMethodNotAllowedHandler,
-		444: defaultConnectionClose,
+		status.BadRequest:            defaultBadRequestHandler,
+		status.NotFound:              defaultNotFoundHandler,
+		status.MethodNotAllowed:      defaultMethodNotAllowedHandler,
+		status.RequestEntityTooLarge: defaultRequestEntityTooLargeHandler,
+		status.ConnectionClose:       defaultConnectionClose,
 	}
 }
 
@@ -43,6 +48,10 @@ func defaultNotFoundHandler(_ *types.Request) types.Response {
 
 func defaultMethodNotAllowedHandler(_ *types.Request) types.Response {
 	return defaultMethodNotAllowed
+}
+
+func defaultRequestEntityTooLargeHandler(_ *types.Request) types.Response {
+	return defaultRequestEntityTooLarge
 }
 
 func defaultConnectionClose(_ *types.Request) types.Response {
