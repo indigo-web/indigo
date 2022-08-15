f63bdf128a6a675030e66049f0b6fa1356f86834
a very fake floordiv <real.floordiv@gmail.com>
2022-08-15 15:08:13 +0200

    updates
    rewritten chunked body parser tests from scratch. Will add more testcases later

--- http/parser/http1/chunkedbodyparser_test.go
+++ http/parser/http1/chunkedbodyparser_test.go
@@ -1,1 +1,45 @@
 package http1
+
+import (
+	"github.com/stretchr/testify/require"
+	"indigo/internal"
+	"indigo/settings"
+	"testing"
+)
+
+func bodyReader(gateway *internal.BodyGateway, ch chan []byte) {
+	var body []byte
+
+	for {
+		data := <-gateway.Data
+		if data == nil {
+			break
+		}
+
+		body = append(body, data...)
+		gateway.Data <- nil
+	}
+
+	ch <- body
+}
+
+func finalize(gateway *internal.BodyGateway) {
+	gateway.Data <- nil
+}
+
+func TestChunkedBodyParser_Parse(t *testing.T) {
+	gateway := internal.NewBodyGateway()
+	ch := make(chan []byte)
+	go bodyReader(gateway, ch)
+
+	chunked := []byte("d\r\nHello, world!\r\n1a\r\nBut what's wrong with you?\r\nf\r\nFinally am here\r\n0\r\n\r\n")
+	wantedBody := "Hello, world!But what's wrong with you?Finally am here"
+	parser := newChunkedBodyParser(gateway, settings.Default())
+
+	done, extra, err := parser.Parse(chunked)
+	require.True(t, done)
+	require.Empty(t, extra)
+	require.NoError(t, err)
+	finalize(gateway)
+	require.Equal(t, wantedBody, string(<-ch))
+}
