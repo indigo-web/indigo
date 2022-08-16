e7591742490dcb72dbc72f813f4cdd4b1c94f063
a very fake floordiv <real.floordiv@gmail.com>
2022-08-16 20:13:34 +0200

    additions
    added a new example with middlewares

---
+++ examples/middlewares/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"indigo"
+	"indigo/router"
+	"indigo/types"
+	"log"
+)
+
+var addr = "localhost:9090"
+
+func MyBeautifulHandler(_ *types.Request) types.Response {
+	fmt.Println("running handler")
+
+	return types.WithResponse
+}
+
+func main() {
+	r := router.NewDefaultRouter()
+
+	api := r.Group("/api")
+	api.Use(HelloWorldMiddleware)
+
+	v1 := api.Group("/v1")
+	v1.Use(SecondMiddleware)
+	v1.Get("/hello", MyBeautifulHandler)
+
+	app := indigo.NewApp(addr)
+	fmt.Println("running on", addr)
+	log.Fatal(app.Serve(r))
+}
