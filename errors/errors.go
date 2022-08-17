d0d355ef76af636350fd3b8c69d5e097469204e5
a very fake floordiv <real.floordiv@gmail.com>
2022-08-17 15:17:21 +0200

    additions
    added 3 more errors

--- errors/errors.go
+++ errors/errors.go
@@ -9,8 +9,12 @@ var (
 	ErrTooLarge            = errors.New("too large")
 	ErrURLTooLong          = errors.New("request URI too long")
 	ErrURLDecoding         = errors.New("invalid url encoding")
+	ErrBadQuery            = errors.New("bad query")
 	ErrUnsupportedProtocol = errors.New("protocol is not supported")
 	ErrTooManyHeaders      = errors.New("too much headers")
 
 	ErrCloseConnection = errors.New("internal error as a signal")
+
+	ErrNoSuchKey = errors.New("requested key is not presented")
+	ErrRead      = errors.New("body has been already read")
 )
