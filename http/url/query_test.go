21cc7b34c673c806bf705145d14dde7698e6f272
a very fake floordiv <real.floordiv@gmail.com>
2022-08-17 16:08:36 +0200

    creations
    written tests for both queries parser and query object

---
+++ http/url/query_test.go
@@ -0,0 +1,28 @@
+package url
+
+import (
+	"github.com/stretchr/testify/require"
+	"indigo/errors"
+	"testing"
+)
+
+func TestQuery(t *testing.T) {
+	// here we test laziness of query
+
+	// just test that passed buffer's content will not be used
+	query := NewQuery([]byte("Hello, world!"))
+	query.Set([]byte("hello=world"))
+	require.Equal(t, "hello=world", string(query.rawQuery))
+	require.Nil(t, query.parsedQuery)
+
+	t.Run("GetExistingKey", func(t *testing.T) {
+		value, err := query.Get("hello")
+		require.NoError(t, err)
+		require.Equal(t, "world", string(value))
+	})
+
+	t.Run("GetNonExistingKey", func(t *testing.T) {
+		_, err := query.Get("lorem")
+		require.ErrorIs(t, err, errors.ErrNoSuchKey)
+	})
+}
