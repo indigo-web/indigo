efe990ade1fd9f35ea1f8d1451fb16f6a41a9b81
a very fake floordiv <real.floordiv@gmail.com>
2022-08-17 16:28:07 +0200

    improvements
    removed useless type `token`, using just a string instead (I started with such an amount of types because of Haskell), implemented re-rendering Accept-Encodings header value each time new decoder is added. We don't care because it is anyway happening in initialisation time

--- http/encodings/contentencodings.go
+++ http/encodings/contentencodings.go
@@ -1,29 +1,36 @@
 package encodings
 
+import (
+	"indigo/http/render"
+	"indigo/internal"
+	"strings"
+)
+
 type (
 	Decoder func([]byte) []byte
-	token   string
 )
 
 type ContentEncodings struct {
-	encodings             map[token]Decoder
+	encodings             map[string]Decoder
 	acceptEncodingsHeader []byte
 }
 
 func NewContentEncodings() ContentEncodings {
 	return ContentEncodings{
-		encodings:             make(map[token]Decoder),
+		encodings:             make(map[string]Decoder),
 		acceptEncodingsHeader: []byte("Accept-Encodings: identity"),
 	}
 }
 
-func (c ContentEncodings) GetDecoder(tok token) (decoder Decoder, found bool) {
-	decoder, found = c.encodings[tok]
+func (c ContentEncodings) GetDecoder(token string) (decoder Decoder, found bool) {
+	decoder, found = c.encodings[token]
 	return decoder, found
 }
 
-func (c ContentEncodings) AddDecoder(tok token, decoder Decoder) {
-	c.encodings[tok] = decoder
+func (c ContentEncodings) AddDecoder(token string, decoder Decoder) {
+	c.encodings[token] = decoder
+	acceptable := strings.Join(internal.Keys(c.encodings), ", ")
+	c.acceptEncodingsHeader = render.Header("Accept-Encodings", acceptable)
 }
 
 func (c ContentEncodings) Acceptable() []byte {
