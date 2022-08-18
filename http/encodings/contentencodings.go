4c7d8bc2b8ffecf7c794f76b052b7b7b1c3443f4
a very fake floordiv <real.floordiv@gmail.com>
2022-08-18 21:53:42 +0200

    improvements
    added docs about purpose of structures and methods and how they may do something

--- http/encodings/contentencodings.go
+++ http/encodings/contentencodings.go
@@ -8,25 +8,39 @@ type (
 	Decoder func([]byte) []byte
 )
 
+// ContentEncodings is just a structure that encapsulates containing
+// content decoders. It does not much but it's honest work
 type ContentEncodings struct {
 	encodings map[string]Decoder
 }
 
+// NewContentEncodings returns new instance of ContentEncodings
 func NewContentEncodings() ContentEncodings {
 	return ContentEncodings{
 		encodings: make(map[string]Decoder),
 	}
 }
 
+// GetDecoder takes a string as an encoding token (name), returns
+// corresponding Decoder
+// in case x-gzip is passed, decoder for gzip is returned, see
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Encoding#directives
 func (c ContentEncodings) GetDecoder(token string) (decoder Decoder, found bool) {
+	if token == "x-gzip" {
+		token = "gzip"
+	}
+
 	decoder, found = c.encodings[token]
 	return decoder, found
 }
 
+// AddDecoder simply adds a new decoder
 func (c ContentEncodings) AddDecoder(token string, decoder Decoder) {
 	c.encodings[token] = decoder
 }
 
+// Acceptable returns a string with all the available decoders, listed
+// by comma
 func (c ContentEncodings) Acceptable() []string {
 	if len(c.encodings) == 0 {
 		return []string{"identity"}
