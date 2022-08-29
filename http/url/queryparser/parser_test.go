dd94585806c1d3e1080b64a2586f1059b844fa8e
a very fake floordiv <real.floordiv@gmail.com>
2022-08-29 11:35:57 +0200

    updates
    due to internal api changes

--- http/url/queryparser/parser_test.go
+++ http/url/queryparser/parser_test.go
@@ -7,10 +7,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func defaultFactory() map[string][]byte {
+	return make(map[string][]byte)
+}
+
 func TestParse_Positive(t *testing.T) {
 	t.Run("OnePair", func(t *testing.T) {
 		query := "hello=world"
-		parsed, err := Parse([]byte(query))
+		parsed, err := Parse([]byte(query), defaultFactory)
 		require.NoError(t, err)
 		require.Contains(t, parsed, "hello")
 		require.Equal(t, "world", string(parsed["hello"]))
@@ -18,7 +22,7 @@ func TestParse_Positive(t *testing.T) {
 
 	t.Run("TwoPairs", func(t *testing.T) {
 		query := "hello=world&lorem=ipsum"
-		parsed, err := Parse([]byte(query))
+		parsed, err := Parse([]byte(query), defaultFactory)
 		require.NoError(t, err)
 		require.Contains(t, parsed, "hello")
 		require.Equal(t, "world", string(parsed["hello"]))
@@ -30,7 +34,7 @@ func TestParse_Positive(t *testing.T) {
 func TestParse_Negative(t *testing.T) {
 	t.Run("EmptyValueBeforeAmpersand", func(t *testing.T) {
 		query := "hello=&another=pair"
-		parsed, err := Parse([]byte(query))
+		parsed, err := Parse([]byte(query), defaultFactory)
 		require.NoError(t, err)
 		require.Contains(t, parsed, "hello")
 		require.Empty(t, parsed["hello"])
@@ -38,7 +42,7 @@ func TestParse_Negative(t *testing.T) {
 
 	t.Run("EmptyValueInTheEnd", func(t *testing.T) {
 		query := "hello="
-		parsed, err := Parse([]byte(query))
+		parsed, err := Parse([]byte(query), defaultFactory)
 		require.NoError(t, err)
 		require.Contains(t, parsed, "hello")
 		require.Empty(t, parsed["hello"])
@@ -46,19 +50,19 @@ func TestParse_Negative(t *testing.T) {
 
 	t.Run("EmptyName", func(t *testing.T) {
 		query := "=world"
-		_, err := Parse([]byte(query))
+		_, err := Parse([]byte(query), defaultFactory)
 		require.ErrorIs(t, err, errors.ErrBadQuery)
 	})
 
 	t.Run("AmpersandInTheEnd", func(t *testing.T) {
 		query := "hello=world&"
-		_, err := Parse([]byte(query))
+		_, err := Parse([]byte(query), defaultFactory)
 		require.ErrorIs(t, err, errors.ErrBadQuery)
 	})
 
 	t.Run("OnlyKeyInTheEnd", func(t *testing.T) {
 		query := "hello=world&lorem"
-		_, err := Parse([]byte(query))
+		_, err := Parse([]byte(query), defaultFactory)
 		require.ErrorIs(t, err, errors.ErrBadQuery)
 	})
 }
