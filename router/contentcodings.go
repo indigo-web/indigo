20b3c7ecb6c037f3ac766139ba3c3ecab0ce5920
a very fake floordiv <real.floordiv@gmail.com>
2022-08-30 13:06:09 +0200

    documentation
    added a note about usage (to be more precise, NOT usage)

--- router/contentcodings.go
+++ router/contentcodings.go
@@ -5,6 +5,9 @@ import "indigo/http/encodings"
 /*
 This file is responsible for adding content-encodings decoders/encoders
 by user, so like custom content-encodings is a way of usage
+
+At the moment is unused because no ideas how to implement it (why tf router
+must do this lol, he does not even access anything for this)
 */
 
 // AddContentEncoding simply adds a new custom decoder
