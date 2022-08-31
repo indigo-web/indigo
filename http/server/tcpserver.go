f7bb39cb607116d615acc9db4c06a0e7aff79570
a very fake floordiv <real.floordiv@gmail.com>
2022-08-31 11:22:38 +0200

    features
    removed deferred connection close because of hijacking chance, so now separately checking error from conn reading and callback (closing err only in case ErrHijackConn is NOT returned)

--- http/server/tcpserver.go
+++ http/server/tcpserver.go
@@ -39,17 +39,24 @@ func StartTCPServer(sock net.Listener, handleConn connHandler, sd chan bool) err
 // DefaultConnHandler is a core handler. It takes a buffer for reading provided
 // by caller, and starts reading from socket. Guarantees:
 // 1) connection will be closed and waitgroup released
+//    * in case callback returned errors.ErrHijackConn, connection will not
+//      be closed
 // 2) on client disconnect, handleData will be called with an empty slice as indicator
 //    of disconnect (normally it is not possible)
 // Errors occurred while reading socket, will be ignored and user will only know that
 // client has disconnected, even if server is guilty
 func DefaultConnHandler(wg *sync.WaitGroup, conn net.Conn, buff []byte, handleData dataHandler) {
-	defer conn.Close()
 	defer wg.Done()
 
 	for {
 		n, err := conn.Read(buff)
-		if handleData(buff[:n]) != nil || n == 0 || err != nil {
+		err2 := handleData(buff[:n])
+
+		if err2 != nil || err != nil || n == 0 {
+			if err2 != errors.ErrHijackConn {
+				conn.Close()
+			}
+
 			return
 		}
 	}
