397ad66108ac579c086bad4aea872ab50080814c
a very fake floordiv <real.floordiv@gmail.com>
2022-09-07 23:34:01 +0200

    docs & features
    added `WithBodyAppend()` and `WithBodyByteAppend()` methods, also written an explaining docstring to every method

--- types/response.go
+++ types/response.go
@@ -28,7 +28,10 @@ type Response struct {
 	Status status.Status
 	// headers due to possible side effects are decided to be private
 	// also uninitialized response must ALWAYS have this value as nil
-	headers  headers.Headers
+	headers headers.Headers
+	// Body is a mutable object. But it's guaranteed that in WithResponse it will not
+	// be modified because it's nil. This means that any data will be appended will
+	// allocate a new underlying array
 	Body     []byte
 	Filename string
 	handler  FileErrHandler
@@ -42,17 +45,25 @@ func NewResponse() Response {
 	}
 }
 
+// WithCode sets a response code and a corresponding status.
+// In case of unknown code, "Unknown Status Code" will be set as a status
+// code. In this case you should call WithStatus explicitly
 func (r Response) WithCode(code status.Code) Response {
 	r.Code = code
 	r.Status = status.Text(code)
 	return r
 }
 
+// WithStatus sets a status text. Not compulsory, because http does not force
+// us strictly checking a response code, and Unknown Status Code is still a
+// valid response code, but you are better to do this. Be a bro
 func (r Response) WithStatus(status status.Status) Response {
 	r.Status = status
 	return r
 }
 
+// WithHeader sets header values to a key. In case it already exists it will
+// be overridden
 func (r Response) WithHeader(key string, values ...string) Response {
 	if r.headers == nil {
 		r.headers = make(headers.Headers, initialRespHeadersSize)
@@ -63,6 +74,7 @@ func (r Response) WithHeader(key string, values ...string) Response {
 	return r
 }
 
+// WithHeaders applies WithHeader to a whole map
 func (r Response) WithHeaders(headers headers.Headers) Response {
 	response := r
 
@@ -73,29 +85,55 @@ func (r Response) WithHeaders(headers headers.Headers) Response {
 	return response
 }
 
+// WithBody sets a string as a response body. This will override already-existing
+// body if it was set
 func (r Response) WithBody(body string) Response {
 	return r.WithBodyByte(internal.S2B(body))
 }
 
+// WithBodyAppend appends a string to already-existing body
+func (r Response) WithBodyAppend(body string) Response {
+	// constructor is anyway non-clear because of headers, so we do not lose
+	// anything
+	r.Body = append(r.Body, body...)
+	return r
+}
+
+// WithBodyByte does all the same as WithBody does, but for byte slices
 func (r Response) WithBodyByte(body []byte) Response {
 	r.Body = body
 	return r
 }
 
+// WithBodyByteAppend does all the same as WithBodyAppend does, but with byte slices
+func (r Response) WithBodyByteAppend(body []byte) Response {
+	r.Body = append(r.Body, body...)
+	return r
+}
+
+// WithFile sets a file path as a file that is supposed to be uploaded as a
+// response. WithFile replaces a response body, so in case last one is specified,
+// it'll be ignored.
+// In case any error occurred (file not found, or error occurred during reading,
+// etc.), handler will be called with a raised error
 func (r Response) WithFile(path string, handler FileErrHandler) Response {
 	r.Filename = path
 	r.handler = handler
 	return r
 }
 
+// WithError simply sets a code status.InternalServerError and response body
+// as an error text
 func (r Response) WithError(err error) Response {
 	return r.WithCode(status.InternalServerError).WithBody(err.Error())
 }
 
+// Headers returns response headers map
 func (r Response) Headers() headers.Headers {
 	return r.headers
 }
 
+// File returns response filename and error handler
 func (r Response) File() (string, FileErrHandler) {
 	return r.Filename, r.handler
 }
