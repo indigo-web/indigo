21cf80122b1a6715ed96a49a0221a5c9bc8398e8
a very fake floordiv <real.floordiv@gmail.com>
2022-09-08 18:52:12 +0200

    updates
    due to headers api changes

--- examples/combined/combined.go
+++ examples/combined/combined.go
@@ -24,7 +24,7 @@ func Index(_ *types.Request) types.Response {
 }
 
 func IndexSay(request *types.Request) types.Response {
-	if value, found := request.Headers["talking"]; !found || value[0] != "allowed" {
+	if talking, found := request.Headers["talking"]; !found || talking[0].Value != "allowed" {
 		return types.WithResponse.
 			WithCode(status.UnavailableForLegalReasons)
 	}
