53c99abaed5c6c09c3c009a0499e0e559fb7160d
a very fake floordiv <real.floordiv@gmail.com>
2022-09-21 00:11:40 +0200

    benchmarks
    added benchmarks for std regexp

--- router/inbuilt/dynmatch/matcher_bench_test.go
+++ router/inbuilt/dynmatch/matcher_bench_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"regexp"
 	"testing"
 )
 
@@ -42,6 +43,71 @@ func BenchmarkMatch(b *testing.B) {
 	b.Run("Long", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			longTemplate.Match(context.Background(), longSample)
+		}
+	})
+}
+
+func BenchmarkRegexp(b *testing.B) {
+	staticSample := `\/hello\/world\/length\/does\/not\/matter$`
+	static, _ := regexp.Compile(staticSample)
+
+	shortTemplateSample := `\/hello\/\w+$`
+	shortSample := "/hello/some-very-long-world"
+	short, _ := regexp.Compile(shortTemplateSample)
+
+	mediumTemplateSample := `\/hello\/\w+/very/\w+/\w+$`
+	mediumSample := "/hello/world-finally-became/very/good-as-fuck/ok-let-it-be"
+	medium, _ := regexp.Compile(mediumTemplateSample)
+
+	longTemplateSample := `\/hello\/\w+\/very\/\w+/\w+\/\w+\/somestatic\/\w+\/good$`
+	longSample := "/hello/world-finally-became/very/good-as-fuck/ok-let-it-be/i-wanna-/somestatic/finally-matcher-is-here/good"
+	long, _ := regexp.Compile(longTemplateSample)
+
+	b.Run("StaticPositive", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			static.MatchString(staticSample)
+		}
+	})
+
+	b.Run("StaticNegative", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			static.MatchString(staticSample + "ok")
+		}
+	})
+
+	b.Run("ShortPositive", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			short.MatchString(shortSample)
+		}
+	})
+
+	b.Run("ShortNegative", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			short.MatchString(shortSample + "ok")
+		}
+	})
+
+	b.Run("MediumPositive", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			medium.MatchString(mediumSample)
+		}
+	})
+
+	b.Run("MediumNegative", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			medium.MatchString(mediumSample + "ok")
+		}
+	})
+
+	b.Run("LongPositive", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			long.MatchString(longSample)
+		}
+	})
+
+	b.Run("LongNegative", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			long.MatchString(longSample + "ok")
 		}
 	})
 }
