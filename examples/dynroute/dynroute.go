4a1cd9e4ad1f6ee76ba8b28a29a43c72649fa008
a very fake floordiv <real.floordiv@gmail.com>
2022-10-02 22:32:42 +0200

    examples
    added an example for dynamic routes

---
+++ examples/dynroute/dynroute.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/fakefloordiv/indigo"
+	"github.com/fakefloordiv/indigo/router/inbuilt"
+	"github.com/fakefloordiv/indigo/types"
+	"log"
+)
+
+var addr = "localhost:9090"
+
+func MyDynamicHandler(ctx context.Context, _ *types.Request) types.Response {
+	worldName := ctx.Value("world-name").(string)
+
+	return types.WithBody("your world-name is " + worldName)
+}
+
+func main() {
+	r := inbuilt.NewRouter()
+
+	r.Get("/hello/{world-name}", MyDynamicHandler)
+
+	fmt.Println("Listening on", addr)
+	app := indigo.NewApp(addr)
+	log.Fatal(app.Serve(r))
+}
