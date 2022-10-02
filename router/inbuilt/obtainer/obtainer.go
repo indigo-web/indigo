5004c379bf6165a950870f1f194530c39ba341e2
a very fake floordiv <real.floordiv@gmail.com>
2022-10-02 22:30:38 +0200

    additions
    added `Auto` method for obtainer that automatically chooses the best obtainer for routes map of possible

--- router/inbuilt/obtainer/obtainer.go
+++ router/inbuilt/obtainer/obtainer.go
@@ -2,8 +2,22 @@ package obtainer
 
 import (
 	"context"
+	"github.com/fakefloordiv/indigo/router/inbuilt/radix"
 	routertypes "github.com/fakefloordiv/indigo/router/inbuilt/types"
 	"github.com/fakefloordiv/indigo/types"
 )
 
 type Obtainer func(context.Context, *types.Request) (context.Context, routertypes.HandlerFunc, error)
+
+// Auto simply checks all the routes, and returns a corresponding obtainer.
+// In case all the routes are static, StaticObtainer is returned.
+// In case at least one route is dynamic, DynamicObtainer is returned
+func Auto(routes routertypes.RoutesMap) Obtainer {
+	for k := range routes {
+		if !radix.MustParse(k).IsStatic() {
+			return DynamicObtainer(routes)
+		}
+	}
+
+	return StaticObtainer(routes)
+}
