3b0d6d08300753cee43c83f3a639afeaf3971b2d
a very fake floordiv <real.floordiv@gmail.com>
2022-10-13 16:19:06 +0200

    fallback
    removed optimisation because it anyway does not makes difference, but leads to data races

--- http/parser/http1/requestsparser.go
+++ http/parser/http1/requestsparser.go
@@ -76,9 +76,7 @@ func (p *httpRequestsParser) Parse(data []byte) (state parser.RequestState, extr
 
 		return parser.ConnectionClose, nil, nil
 	}
-
-	_ = *p.request
-
+	
 	if p.state == eBody {
 		var done bool
 		done, extra, err = p.parseBody(data)
