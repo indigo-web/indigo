8428907395e8ab2dccc56f03425de1e58bdd245b
a very fake floordiv <real.floordiv@gmail.com>
2022-11-15 15:38:43 +0100

    updates: new types, updated old types
    added a new type - `ErrHandlers`, updated `HandlerFunc` and `Middleware` types (removed context as an argument)

--- router/inbuilt/types/types.go
+++ router/inbuilt/types/types.go
@@ -1,18 +1,18 @@
 package types
 
 import (
-	"context"
+	"github.com/fakefloordiv/indigo/http"
 
 	methods "github.com/fakefloordiv/indigo/http/method"
-	"github.com/fakefloordiv/indigo/types"
 )
 
 type (
-	HandlerFunc func(context.Context, *types.Request) types.Response
+	HandlerFunc func(*http.Request) http.Response
+	ErrHandlers map[error]HandlerFunc
 	// Middleware works like a chain of nested calls, next may be even directly
 	// handler. But if we are not a closing middleware, we will call next
 	// middleware that is simply a partial middleware with already provided next
-	Middleware func(ctx context.Context, next HandlerFunc, request *types.Request) types.Response
+	Middleware func(next HandlerFunc, request *http.Request) http.Response
 )
 
 type (
