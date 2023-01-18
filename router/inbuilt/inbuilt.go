1f7517a8c95e9bf8ef0913fb52bc1551579dc8a0
a very fake floordiv <real.floordiv@gmail.com>
2023-01-18 22:40:03 +0100

    updated comments
    updated information provided by comment for Router struct

--- router/inbuilt/inbuilt.go
+++ router/inbuilt/inbuilt.go
@@ -5,14 +5,10 @@ import (
 	routertypes "github.com/fakefloordiv/indigo/router/inbuilt/types"
 )
 
-// Router is a reference implementation of router for indigo
-// It supports:
-// 1) Endpoint groups
-// 2) Middlewares
-// 3) Error handlers
-// 4) Encoding/decoding incoming content
-// 5) Routing by path and method. If path not found, 404 Not Found is returned. If path
-// is found, but no method attached, 413 Method Not Allowed is returned.
+// Router is a built-in implementation of router.Router interface that provides
+// some basic router features like middlewares, groups, dynamic routing, error
+// handlers, and some implicit things like calling GET-handlers for HEAD-requests,
+// or rendering TRACE-responses automatically in case no handler is registered
 type Router struct {
 	root   *Router
 	groups []Router
