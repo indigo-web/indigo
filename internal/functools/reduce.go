166427ba3acc25f1df3ec13c01ba8a06cd684282
a very fake floordiv <real.floordiv@gmail.com>
2023-01-18 22:46:05 +0100

    functools expansion
    moooore functools! Added Reduce and Sum functions (they're used only once, but tbh their usage looks beautifully)

---
+++ internal/functools/reduce.go
@@ -0,0 +1,26 @@
+package functools
+
+import "github.com/fakefloordiv/indigo/internal/constraints"
+
+func Reduce[T constraints.Addable](f func(T, T) T, input []T, initial ...T) (result T) {
+	switch len(initial) {
+	case 0:
+	case 1:
+		result = initial[0]
+	default:
+		panic("reduce: cannot pass more than one initial value")
+	}
+
+	switch len(input) {
+	case 0:
+		return result
+	case 1:
+		return input[0]
+	}
+
+	for _, element := range input {
+		result = f(result, element)
+	}
+
+	return result
+}
