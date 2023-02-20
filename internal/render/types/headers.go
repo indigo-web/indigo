0580df8989bbfed317303b0406a88a252a6868dc
fakefloordiv <real.floordiv@gmail.com>
2023-02-21 00:19:19 +0100

    a type for default headers
    only thing it does - simplifies the process of fetching default headers by implementing `EraseEntry()` method, that nulls a key if it equals to desired one

---
+++ internal/render/types/headers.go
@@ -0,0 +1,20 @@
+package types
+
+type DefaultHeaders []string
+
+// EraseEntry nulls key in case it is presented, if not - nothing happens
+func (d DefaultHeaders) EraseEntry(key string) {
+	if len(d) == 0 {
+		return
+	}
+
+	for i := 0; i <= len(d)/2; i += 2 {
+		if d[i] == key {
+			d[i] = ""
+		}
+	}
+}
+
+func (d DefaultHeaders) Copy(into []string) {
+	copy(into, d)
+}
