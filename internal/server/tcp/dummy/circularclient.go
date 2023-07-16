dd3e20886cd6ad55be8248758a0f549dc41e3423
fakefloordiv <real.floordiv@gmail.com>
2023-07-16 04:11:57 +0200

    added the option of being one-time client

--- internal/server/tcp/dummy/circularclient.go
+++ internal/server/tcp/dummy/circularclient.go
@@ -4,30 +4,32 @@ import (
 	"github.com/indigo-web/indigo/internal/unreader"
 	"io"
 	"net"
-
-	"github.com/indigo-web/indigo/internal/server/tcp"
 )
 
-// circularClient is a client that on every read-operation returns the same data as it
+// CircularClient is a client that on every read-operation returns the same data as it
 // was initialised with. This is used mainly for benchmarking
-type circularClient struct {
-	unreader *unreader.Unreader
-	data     [][]byte
-	pointer  int
-	closed   bool
+type CircularClient struct {
+	unreader        *unreader.Unreader
+	data            [][]byte
+	pointer         int
+	closed, oneTime bool
 }
 
-func NewCircularClient(data ...[]byte) tcp.Client {
-	return &circularClient{
+func NewCircularClient(data ...[]byte) *CircularClient {
+	return &CircularClient{
 		unreader: new(unreader.Unreader),
 		data:     data,
 		pointer:  -1,
 	}
 }
 
-func (c *circularClient) Read() ([]byte, error) {
+func (c *CircularClient) Read() ([]byte, error) {
 	if c.closed {
 		return nil, io.EOF
+	}
+
+	if c.oneTime {
+		c.closed = true
 	}
 
 	return c.unreader.PendingOr(func() ([]byte, error) {
@@ -41,19 +43,23 @@ func (c *circularClient) Read() ([]byte, error) {
 	})
 }
 
-func (c *circularClient) Unread(takeback []byte) {
+func (c *CircularClient) Unread(takeback []byte) {
 	c.unreader.Unread(takeback)
 }
 
-func (*circularClient) Write([]byte) error {
+func (*CircularClient) Write([]byte) error {
 	return nil
 }
 
-func (*circularClient) Remote() net.Addr {
+func (*CircularClient) Remote() net.Addr {
 	return nil
 }
 
-func (c *circularClient) Close() error {
+func (c *CircularClient) Close() error {
 	c.closed = true
 	return nil
+}
+
+func (c *CircularClient) OneTime() {
+	c.oneTime = true
 }
