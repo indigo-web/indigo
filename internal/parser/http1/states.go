219095e1d93d0db744e1d585b1e63c86effc79e5
fakefloordiv <real.floordiv@gmail.com>
2023-08-01 15:09:15 +0200

    removed unused states

--- internal/parser/http1/states.go
+++ internal/parser/http1/states.go
@@ -5,24 +5,6 @@ type parserState uint8
 const (
 	eMethod parserState = iota + 1
 	ePath
-	ePathDecode1Char
-	ePathDecode2Char
-	eQuery
-	eQueryDecode1Char
-	eQueryDecode2Char
-	eFragment
-	eProto
-	eH
-	eHT
-	eHTT
-	eHTTP
-	eProtoMajor
-	eProtoDot
-	eProtoMinor
-	eProtoEnd
-	eProtoCR
-	eProtoCRLF
-	eProtoCRLFCR
 	eHeaderKey
 	eContentLength
 	eContentLengthCR
