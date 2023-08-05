3b33424caf4e2ef17558ffc88b50be20455cf603
fakefloordiv <real.floordiv@gmail.com>
2023-08-05 21:34:09 +0200

    removed HandlerObject, renamed HandlerFunc -> Handler

--- router/inbuilt/shorthands.go
+++ router/inbuilt/shorthands.go
@@ -11,55 +11,55 @@ with already set method taken from name of the method
 */
 
 // Get is a shortcut for registering GET-requests
-func (r *Router) Get(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Get(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.GET, path, handler, middlewares...)
 	return r
 }
 
 // Head is a shortcut for registering HEAD-requests
-func (r *Router) Head(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Head(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.HEAD, path, handler, middlewares...)
 	return r
 }
 
 // Post is a shortcut for registering POST-requests
-func (r *Router) Post(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Post(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.POST, path, handler, middlewares...)
 	return r
 }
 
 // Put is a shortcut for registering PUT-requests
-func (r *Router) Put(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Put(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.PUT, path, handler, middlewares...)
 	return r
 }
 
 // Delete is a shortcut for registering DELETE-requests
-func (r *Router) Delete(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Delete(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.DELETE, path, handler, middlewares...)
 	return r
 }
 
 // Connect is a shortcut for registering CONNECT-requests
-func (r *Router) Connect(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Connect(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.CONNECT, path, handler, middlewares...)
 	return r
 }
 
 // Options is a shortcut for registering OPTIONS-requests
-func (r *Router) Options(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Options(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.OPTIONS, path, handler, middlewares...)
 	return r
 }
 
 // Trace is a shortcut for registering TRACE-requests
-func (r *Router) Trace(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Trace(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.TRACE, path, handler, middlewares...)
 	return r
 }
 
 // Patch is a shortcut for registering PATCH-requests
-func (r *Router) Patch(path string, handler types.HandlerFunc, middlewares ...types.Middleware) *Router {
+func (r *Router) Patch(path string, handler types.Handler, middlewares ...types.Middleware) *Router {
 	r.Route(method.PATCH, path, handler, middlewares...)
 	return r
 }
--- router/inbuilt/types/types.go
+++ router/inbuilt/types/types.go
@@ -2,25 +2,15 @@ package types
 
 import (
 	"github.com/indigo-web/indigo/http"
-
 	"github.com/indigo-web/indigo/http/method"
 )
 
 type (
-	HandlerFunc func(*http.Request) http.Response
-	ErrHandlers map[error]HandlerFunc
+	Handler     func(*http.Request) http.Response
+	ErrHandlers map[error]Handler
 	// Middleware works like a chain of nested calls, next may be even directly
 	// handler. But if we are not a closing middleware, we will call next
 	// middleware that is simply a partial middleware with already provided next
-	Middleware func(next HandlerFunc, request *http.Request) http.Response
-)
-
-type (
-	MethodsMap [method.Count]*HandlerObject
-	RoutesMap  map[string]MethodsMap
-
-	HandlerObject struct {
-		Fun         HandlerFunc
-		Middlewares []Middleware
-	}
+	Middleware func(next Handler, request *http.Request) http.Response
+	MethodsMap [method.Count]Handler
 )
