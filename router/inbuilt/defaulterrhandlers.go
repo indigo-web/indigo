c007e398a8b9c84641b6ba713ef3afcfb25fbdc3
fakefloordiv <real.floordiv@gmail.com>
2023-08-05 21:36:58 +0200

    minor code improvement

--- router/inbuilt/defaulterrhandlers.go
+++ router/inbuilt/defaulterrhandlers.go
@@ -22,7 +22,7 @@ func newErrorHandlers() types.ErrHandlers {
 }
 
 func defaultMethodNotAllowedHandler(request *http.Request) http.Response {
-	response := http.RespondTo(request).WithError(status.ErrMethodNotAllowed)
+	response := request.Respond().WithError(status.ErrMethodNotAllowed)
 
 	if allow, ok := request.Ctx.Value("allow").(string); ok {
 		response = response.WithHeader("Allow", allow)
