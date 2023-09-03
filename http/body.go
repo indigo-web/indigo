5896551d321f2edc503b11acc11b0bdc6de5e429
fakefloordiv <real.floordiv@gmail.com>
2023-09-04 00:09:56 +0200

    removed Raw* methods, fixed io.Reader implementation
    now io.Reader implementation isn't that buggy anymore, and doesn't return more n than consumer can consume. Renamed Value() -> Full()

--- http/body.go
+++ http/body.go
@@ -1,8 +1,8 @@
 package http
 
 import (
-	"github.com/indigo-web/indigo/http/decode"
 	"github.com/indigo-web/indigo/http/headers"
+	"github.com/indigo-web/indigo/internal/unreader"
 	"io"
 )
 
@@ -18,35 +18,43 @@ var _ io.Reader = &Body{}
 
 type Body struct {
 	reader        BodyReader
-	decoder       *decode.Decoder
-	te            headers.TransferEncoding
+	unreader      unreader.Unreader
+	te            headers.Encoding
 	contentLength int
 	bodyBuff      []byte
-	rawIOReader   bodyIOReader
 }
 
-func NewBody(reader BodyReader, decoder *decode.Decoder) *Body {
+func NewBody(reader BodyReader) *Body {
 	return &Body{
-		reader:      reader,
-		decoder:     decoder,
-		rawIOReader: newBodyIOReader(),
+		reader: reader,
 	}
 }
+
+const chunkedTE = -1
 
 func (b *Body) Init(req *Request) {
 	b.reader.Init(req)
-	b.te = req.TransferEncoding
+	b.unreader.Reset()
+	b.te = req.Encoding
 	b.contentLength = req.ContentLength
 
-	if req.TransferEncoding.Chunked {
-		b.contentLength = -1
+	if req.Encoding.Chunked {
+		b.contentLength = chunkedTE
 	}
 }
 
-// Raw returns RAW representation of the body. This means, that it may be encoded. If you
-// want a decoded body, then use Value() method instead
-func (b *Body) Raw() ([]byte, error) {
-	if b.bodyBuff == nil && b.contentLength > 0 {
+// Full returns the whole body at once
+//
+// WARNING: returned slice is an underlying buffer, that may be rewritten in a next request.
+// If you want to save a content somewhere, you have to make sure that it'll be copied into
+// another memory location
+func (b *Body) Full() ([]byte, error) {
+	// in case transfer-encoding is chunked, this condition won't be satisfied. In this case,
+	// the buffer may still be nil, that'll cause many re-allocations. The problem is, that
+	// chunked body size median in production is unknown (and usually varies across projects).
+	// Maybe, we could add an option to the settings to pre-allocate the buffer in such cases,
+	// but this will affect only cold-start stages. Not sure, whether it's time-worthy
+	if len(b.bodyBuff) < b.contentLength {
 		b.bodyBuff = make([]byte, 0, b.contentLength)
 	}
 
@@ -58,54 +66,38 @@ func (b *Body) Raw() ([]byte, error) {
 	})
 }
 
-// Value returns decoded full body of the request
-func (b *Body) Value() ([]byte, error) {
-	rawData, err := b.Raw()
-	if err != nil {
-		return nil, err
-	}
-
-	return b.decoder.Decode(b.te.Token, rawData)
-}
-
 // Read implements the io.Reader interface, so behaves respectively
-func (b *Body) Read(buff []byte) (n int, err error) {
-	rawData, err := b.reader.Read()
+func (b *Body) Read(into []byte) (n int, err error) {
+	data, err := b.unreader.PendingOr(b.reader.Read)
 	if err != nil {
 		return 0, err
 	}
 
-	data, err := b.decoder.Decode(b.te.Token, rawData)
-	copy(buff, data)
+	n = copy(into, data)
+	if len(data) > n {
+		b.unreader.Unread(data[n:])
+	}
 
-	return len(data), err
-}
-
-// RawReader returns io.Reader implementation in order to read directly from the input
-// stream. The stream may be encoded, so highly recommended just to use the Body entity
-// as io.Reader implementation
-func (b *Body) RawReader() io.Reader {
-	b.rawIOReader.Reassign(b.reader)
-
-	return b.rawIOReader
+	return n, err
 }
 
 // Callback takes a function, that'll be called with body piece every time it's received.
 // In case error is returned from the callback, it'll also be returned from this method
 func (b *Body) Callback(onBody onBodyCallback) error {
-	return b.callback(func(data []byte) error {
-		decoded, err := b.decoder.Decode(b.te.Token, data)
-		if err != nil {
-			return err
-		}
-
-		return onBody(decoded)
-	})
+	return b.callback(onBody)
 }
 
-// RawCallback does the same as Callback, but body pieces aren't prematurely decoded.
-func (b *Body) RawCallback(onBody onBodyCallback) error {
-	return b.callback(onBody)
+func (b *Body) Reset() error {
+	for {
+		_, err := b.reader.Read()
+		switch err {
+		case nil:
+		case io.EOF:
+			return nil
+		default:
+			return err
+		}
+	}
 }
 
 func (b *Body) callback(onBody onBodyCallback) error {
@@ -118,20 +110,8 @@ func (b *Body) callback(onBody onBodyCallback) error {
 		default:
 			return err
 		}
-		if err = onBody(piece); err != nil {
-			return err
-		}
-	}
-}
 
-func (b *Body) Reset() error {
-	for {
-		_, err := b.reader.Read()
-		switch err {
-		case nil:
-		case io.EOF:
-			return nil
-		default:
+		if err = onBody(piece); err != nil {
 			return err
 		}
 	}
