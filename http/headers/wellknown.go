e19878a13939f3606a5b4a780dbb72abe96c7e84
A Really Fake Floordiv Since 4th January, 2022 <97134096+fakefloordiv@users.noreply.github.com>
2023-09-04 15:08:18 +0200

    updated headers.Encoding structure
    made Transfer and Content fields not anonymous structures wrapping a single strings slice, but simply string slices

--- http/headers/wellknown.go
+++ http/headers/wellknown.go
@@ -1,16 +1,10 @@
 package headers
 
 type Encoding struct {
-	// Transfer represents Transfer-Encoding header value
-	Transfer struct {
-		Tokens []string
-	}
-
-	// Content represents Content-Encoding header value
-	Content struct {
-		Tokens []string
-	}
-
+	// Transfer represents Transfer-Encoding header value, split by comma
+	Transfer []string
+	// Content represents Content-Encoding header value, split by comma
+	Content []string
 	// Chunked doesn't belong to any of encodings, as it is still must be processed individually
 	Chunked, HasTrailer bool
 }
