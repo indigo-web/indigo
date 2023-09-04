e359cb279ba5fae4b96213b4eee1d56699b2d39f
A Really Fake Floordiv Since 4th January, 2022 <97134096+fakefloordiv@users.noreply.github.com>
2023-09-04 11:38:49 +0200

    updated Encoding type
    now it contains both Transfer- and Content-Encodings, but separated

--- http/headers/wellknown.go
+++ http/headers/wellknown.go
@@ -1,6 +1,16 @@
 package headers
 
 type Encoding struct {
+	// Transfer represents Transfer-Encoding header value
+	Transfer struct {
+		Tokens []string
+	}
+
+	// Content represents Content-Encoding header value
+	Content struct {
+		Tokens []string
+	}
+
+	// Chunked doesn't belong to any of encodings, as it is still must be processed individually
 	Chunked, HasTrailer bool
-	Tokens              []string
 }
