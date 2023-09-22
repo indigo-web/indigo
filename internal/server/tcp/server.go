16ce027f89a0104f7dec411f53290aa40969654a
A Really Fake Floordiv Since 4th January, 2022 <97134096+fakefloordiv@users.noreply.github.com>
2023-09-22 09:24:46 +0200

    fixed memory leaking
    fixed memory leaking, when conn objects after their closing weren't deleted from a map

--- internal/server/tcp/server.go
+++ internal/server/tcp/server.go
@@ -23,40 +23,40 @@ func NewServer(sock net.Listener, onConn onConnection) *Server {
 	}
 }
 
-func (t *Server) Start() error {
+func (s *Server) Start() error {
 	wg := new(sync.WaitGroup)
 
 	for {
-		conn, err := t.sock.Accept()
+		conn, err := s.sock.Accept()
 		if err != nil {
 			wg.Wait()
 
-			if t.shutdown {
+			if s.shutdown {
 				return status.ErrShutdown
 			}
 
 			return err
 		}
 
-		t.conns[conn] = struct{}{}
+		s.conns[conn] = struct{}{}
 		wg.Add(1)
-		go connHandler(wg, conn, t.onConn)
+		go s.connHandler(wg, conn)
 	}
 }
 
-func (t *Server) stopListener() error {
-	t.shutdown = true
+func (s *Server) stopListener() error {
+	s.shutdown = true
 
-	return t.sock.Close()
+	return s.sock.Close()
 }
 
 // Stop shuts listener and ALL the connections down
-func (t *Server) Stop() error {
-	if err := t.stopListener(); err != nil {
+func (s *Server) Stop() error {
+	if err := s.stopListener(); err != nil {
 		return err
 	}
 
-	for conn := range t.conns {
+	for conn := range s.conns {
 		_ = conn.Close()
 	}
 
@@ -65,11 +65,12 @@ func (t *Server) Stop() error {
 
 // GracefulShutdown stops a listener, but leaving all the connections free to end their
 // lives peacefully
-func (t *Server) GracefulShutdown() error {
-	return t.stopListener()
+func (s *Server) GracefulShutdown() error {
+	return s.stopListener()
 }
 
-func connHandler(wg *sync.WaitGroup, conn net.Conn, onConn onConnection) {
-	onConn(conn)
+func (s *Server) connHandler(wg *sync.WaitGroup, conn net.Conn) {
+	s.onConn(conn)
 	wg.Done()
+	delete(s.conns, conn)
 }
