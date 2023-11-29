8e11cb899f5c99fda4c84b9247f7839103804f6b
fakefloordiv <real.floordiv@gmail.com>
2023-11-30 00:43:54 +0100

    updated static files distribution
    now if requested path is a directory, index.html from it is returned if exists

--- router/inbuilt/static.go
+++ router/inbuilt/static.go
@@ -3,22 +3,41 @@ package inbuilt
 import (
 	"github.com/indigo-web/indigo/http"
 	"github.com/indigo-web/indigo/router/inbuilt/types"
+	"os"
 	"strings"
 )
 
 // Static adds a catcher of prefix, that automatically returns files from defined root
 // directory
 func (r *Router) Static(prefix, root string, mwares ...types.Middleware) *Router {
-	prefix = mustTrailingSlash(prefix)
+	prefix = stripTrailingSlash(prefix)
 	root = mustTrailingSlash(root)
 
 	return r.Catch(prefix, func(request *http.Request) *http.Response {
-		path := strings.TrimPrefix(request.Path, prefix)
+		path := root + stripLeadingSlash(strings.TrimPrefix(request.Path, prefix))
+		stat, err := os.Stat(path)
+		if err != nil {
+			return request.Respond().WithError(err)
+		}
 
-		return request.Respond().WithFile(root + path)
+		if stat.IsDir() {
+			path += "/index.html"
+		}
+
+		return request.Respond().WithFile(path)
 	}, mwares...)
 }
 
 func mustTrailingSlash(path string) string {
 	return stripTrailingSlash(path) + "/"
+}
+
+func stripLeadingSlash(path string) string {
+	for i := 0; i < len(path); i++ {
+		if path[i] != '/' {
+			return path[i:]
+		}
+	}
+
+	return ""
 }
