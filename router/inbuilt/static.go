d47e1f44726a6ef90d5fbdbf3cdc38e6bb2c2b86
fakefloordiv <real.floordiv@gmail.com>
2023-12-01 14:41:54 +0100

    using pathlib in static files distributor
    as previous variant with self-made strings operations was too fragile and difficult to maintain, I added a pathlib that is doing all the job with paths prefixes replacing

--- router/inbuilt/static.go
+++ router/inbuilt/static.go
@@ -2,42 +2,18 @@ package inbuilt
 
 import (
 	"github.com/indigo-web/indigo/http"
+	"github.com/indigo-web/indigo/internal/pathlib"
 	"github.com/indigo-web/indigo/router/inbuilt/types"
-	"os"
-	"strings"
 )
 
 // Static adds a catcher of prefix, that automatically returns files from defined root
 // directory
 func (r *Router) Static(prefix, root string, mwares ...types.Middleware) *Router {
-	prefix = stripTrailingSlash(prefix)
-	root = mustTrailingSlash(root)
+	pathReplacer := pathlib.NewPath(prefix, root)
 
 	return r.Catch(prefix, func(request *http.Request) *http.Response {
-		path := root + stripLeadingSlash(strings.TrimPrefix(request.Path, prefix))
-		stat, err := os.Stat(path)
-		if err != nil {
-			return request.Respond().WithError(err)
-		}
+		pathReplacer.Set(request.Path)
 
-		if stat.IsDir() {
-			path += "/index.html"
-		}
-
-		return request.Respond().WithFile(path)
+		return request.Respond().WithFile(pathReplacer.Relative())
 	}, mwares...)
-}
-
-func mustTrailingSlash(path string) string {
-	return stripTrailingSlash(path) + "/"
-}
-
-func stripLeadingSlash(path string) string {
-	for i := 0; i < len(path); i++ {
-		if path[i] != '/' {
-			return path[i:]
-		}
-	}
-
-	return ""
 }
