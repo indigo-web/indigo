5207c65b7e054ae915b4bd352160225c1dbbe2d8
fakefloordiv <real.floordiv@gmail.com>
2023-12-03 13:25:16 +0100

    added a few new handy middlewares

---
+++ router/inbuilt/middleware/logging.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"github.com/indigo-web/indigo/http"
+	"github.com/indigo-web/indigo/router/inbuilt"
+	"log"
+)
+
+type Logger interface {
+	Printf(fmt string, v ...any)
+}
+
+func LogRequests(loggers ...Logger) inbuilt.Middleware {
+	if len(loggers) == 0 {
+		loggers = append(loggers, log.Default())
+	}
+
+	return func(next inbuilt.Handler, request *http.Request) *http.Response {
+		response := next(request)
+
+		for _, logger := range loggers {
+			logger.Printf("%s %s %d", request.Method.String(), request.Path, response.Reveal().Code)
+		}
+
+		return response
+	}
+}
