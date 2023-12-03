e648018339235c0a2d19b333f327e2f3d1ce4bab
fakefloordiv <real.floordiv@gmail.com>
2023-12-03 13:27:10 +0100

    updated existing middlewares due to api changes

--- router/inbuilt/middleware/recover.go
+++ router/inbuilt/middleware/recover.go
@@ -3,12 +3,12 @@ package middleware
 import (
 	"github.com/indigo-web/indigo/http"
 	"github.com/indigo-web/indigo/http/status"
-	"github.com/indigo-web/indigo/router/inbuilt/types"
+	"github.com/indigo-web/indigo/router/inbuilt"
 	"log"
 )
 
 // Recover is a basic middleware that catches any panics, logs it and returns 500 Internal Server Error
-func Recover(next types.Handler, req *http.Request) (resp *http.Response) {
+func Recover(next inbuilt.Handler, req *http.Request) (resp *http.Response) {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Printf("panic: %v\n", r)
