20fe344039810a39de81a817c5204a0179d92bcc
a very fake floordiv <real.floordiv@gmail.com>
2023-12-07 18:55:53 +0100

    completed and optimised the flect

--- internal/flect/flect.go
+++ internal/flect/flect.go
@@ -1,19 +1,13 @@
-package main
+package flect
 
 import (
-	"fmt"
 	"reflect"
 	"unsafe"
 )
 
-type MyStruct struct {
-	A uint8
-	B uint64
-	C uint8
-}
-
-func (m MyStruct) String() string {
-	return fmt.Sprintf("MyStruct{%d, %d, %d}", m.A, m.B, m.C)
+type Field struct {
+	Key  string
+	Data unsafe.Pointer
 }
 
 type fieldData struct {
@@ -21,8 +15,7 @@ type fieldData struct {
 }
 
 type Model[T any] struct {
-	offsets     map[string]fieldData
-	offsetsInts []fieldData
+	offsets *attrsMap
 }
 
 func NewModel[T any]() Model[T] {
@@ -33,41 +26,29 @@ func NewModel[T any]() Model[T] {
 	}
 
 	model := Model[T]{
-		offsets: map[string]fieldData{},
+		offsets: new(attrsMap),
 	}
 
 	for i := 0; i < typ.NumField(); i++ {
 		field := typ.Field(i)
-		model.offsetsInts = append(model.offsetsInts, fieldData{
+		model.offsets.Insert(field.Name, fieldData{
 			Size:   field.Type.Size(),
 			Offset: field.Offset,
 		})
-		model.offsets[field.Name] = fieldData{
-			Size:   field.Type.Size(),
-			Offset: field.Offset,
-		}
 	}
 
 	return model
 }
 
-type FieldPair struct {
-	Key  string
-	Data unsafe.Pointer
-}
-
-func (m Model[T]) WriteUInt8(into T, key string, num uint8) T {
-	field := m.offsets[key]
-	memcpy(unsafe.Add(unsafe.Pointer(&into), field.Offset), unsafe.Pointer(&num), field.Size)
-	return into
-}
-
-func (m Model[T]) New(values []FieldPair) T {
+func (m Model[T]) Instantiate(values []Field) T {
 	var zero T
 	zeroUPtr := unsafe.Pointer(&zero)
 
 	for _, value := range values {
-		field := m.offsets[value.Key]
+		field, found := m.offsets.Lookup(value.Key)
+		if !found {
+			continue
+		}
 		fieldPtr := unsafe.Add(zeroUPtr, field.Offset)
 		memcpy(fieldPtr, value.Data, field.Size)
 	}
@@ -75,24 +56,47 @@ func (m Model[T]) New(values []FieldPair) T {
 	return zero
 }
 
+func (m Model[T]) write(into T, key string, val unsafe.Pointer, size uintptr) T {
+	field, found := m.offsets.Lookup(key)
+	if !found {
+		return into
+	}
+	memcpy(unsafe.Add(unsafe.Pointer(&into), field.Offset), val, size)
+	return into
+}
+
+func (m Model[T]) WriteUInt8(into T, key string, num uint8) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteUInt16(into T, key string, num uint16) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteUInt32(into T, key string, num uint32) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteUInt64(into T, key string, num uint64) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteInt8(into T, key string, num int8) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteInt16(into T, key string, num int16) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteInt32(into T, key string, num int32) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
+func (m Model[T]) WriteInt64(into T, key string, num int64) T {
+	return m.write(into, key, unsafe.Pointer(&num), unsafe.Sizeof(num))
+}
+
 func memcpy(dst, src unsafe.Pointer, size uintptr) {
 	copy(unsafe.Slice((*byte)(dst), size), unsafe.Slice((*byte)(src), size))
-}
-
-func main() {
-	model := NewModel[MyStruct]()
-	fmt.Println("model:", model)
-	m := MyStruct{}
-	updated := model.WriteUInt8(m, "A", 87)
-	fmt.Println(updated.String())
-	filled := model.New([]FieldPair{
-		{"A", uptr(1)},
-		{"B", uptr(1234567)},
-		{"C", uptr(5)},
-	})
-	fmt.Println(filled.String())
-}
-
-func uptr[T any](val T) unsafe.Pointer {
-	return unsafe.Pointer(&val)
 }
