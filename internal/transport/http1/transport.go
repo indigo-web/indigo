ce3cf6784f82c370aa3b7a141fcb8d163a34a91c
a very fake floordiv <real.floordiv@gmail.com>
2024-01-11 20:40:19 +0100

    now Transport struct embeds parser and serializer instead of providing proxy-methods

--- internal/transport/http1/transport.go
+++ internal/transport/http1/transport.go
@@ -2,16 +2,17 @@ package http1
 
 import (
 	"github.com/indigo-web/indigo/http"
-	"github.com/indigo-web/indigo/http/proto"
 	"github.com/indigo-web/indigo/internal/transport"
 	"github.com/indigo-web/indigo/settings"
 	"github.com/indigo-web/utils/buffer"
 	"github.com/indigo-web/utils/pool"
 )
 
+var _ transport.Transport = new(Transport)
+
 type Transport struct {
-	parser *Parser
-	dumper *Dumper
+	*Parser
+	*Serializer
 }
 
 func New(
@@ -24,19 +25,7 @@ func New(
 	defaultHeaders map[string]string,
 ) *Transport {
 	return &Transport{
-		parser: NewParser(request, keyBuff, valBuff, startLineBuff, valuesPool, headersSettings),
-		dumper: NewDumper(respBuff, respFileBuffSize, defaultHeaders),
+		Parser:     NewParser(request, keyBuff, valBuff, startLineBuff, valuesPool, headersSettings),
+		Serializer: NewSerializer(respBuff, respFileBuffSize, defaultHeaders),
 	}
-}
-
-func (t *Transport) Parse(b []byte) (state transport.RequestState, extra []byte, err error) {
-	return t.parser.Parse(b)
-}
-
-func (t *Transport) PreDump(target proto.Proto, response *http.Response) {
-	t.dumper.PreDump(target, response)
-}
-
-func (t *Transport) Dump(target proto.Proto, request *http.Request, response *http.Response, writer transport.Writer) error {
-	return t.dumper.Dump(target, request, response, writer)
 }
