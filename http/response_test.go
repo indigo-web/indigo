442538afd1318fa7afef773d65c2d49c09dd9487
a very fake floordiv <real.floordiv@gmail.com>
2024-01-12 14:13:58 +0100

    fix due to api changes

--- http/response_test.go
+++ http/response_test.go
@@ -9,7 +9,7 @@ func TestResponse(t *testing.T) {
 	t.Run("JSON", func(t *testing.T) {
 		response := NewResponse()
 		m := []int{1, 2, 3}
-		resp, err := response.JSON(m)
+		resp, err := response.TryJSON(m)
 		require.NoError(t, err)
 		require.Equal(t, "[1,2,3]", string(resp.Reveal().Body))
 		require.Equal(t, "application/json", resp.Reveal().ContentType)
