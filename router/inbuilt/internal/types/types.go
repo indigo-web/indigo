967c6a4df778866058d91b8c48a702d82b286f47
fakefloordiv <real.floordiv@gmail.com>
2024-02-12 14:04:10 +0100

    remove ugly named return in Handler type

--- router/inbuilt/internal/types/types.go
+++ router/inbuilt/internal/types/types.go
@@ -8,7 +8,7 @@ import (
 type (
 	// Handler is a function for processing a request. Using named return as
 	// underscore just in order to be able to make an empty return
-	Handler    func(*http.Request) (_ *http.Response)
+	Handler    func(*http.Request) *http.Response
 	MethodsMap [method.Count + 1]Handler
 	Mutator    func(request *http.Request)
 )
