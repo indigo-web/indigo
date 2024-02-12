f01e6d5ab83831c5f8f092e509ea0217d094814a
fakefloordiv <real.floordiv@gmail.com>
2024-02-12 14:03:39 +0100

    re-formatted the address package for new approaches

--- internal/address/address.go
+++ internal/address/address.go
@@ -1,64 +1,23 @@
 package address
 
 import (
-	"fmt"
-	"math"
-	"strconv"
+	"net"
 	"strings"
 )
 
-// DefaultHost used in cases, when no host was defined. For example, the string ":8080"
-const DefaultHost = "0.0.0.0"
-
-type Address struct {
-	Host string
-	Port uint16
+func IsLocalhost(addr string) bool {
+	return strings.EqualFold(removePort(addr), "localhost")
 }
 
-func Parse(addr string) (address Address, err error) {
+func IsIP(addr string) bool {
+	return net.ParseIP(removePort(addr)) != nil
+}
+
+func removePort(addr string) string {
 	colon := strings.IndexByte(addr, ':')
-	if colon == -1 {
-		return address, fmt.Errorf("no port given")
+	if colon != -1 {
+		return addr[:colon]
 	}
 
-	host, rawPort := addr[:colon], addr[colon+1:]
-	if len(host) == 0 {
-		host = DefaultHost
-	}
-	if len(rawPort) == 0 {
-		return address, fmt.Errorf("port cannot be empty")
-	}
-
-	port, err := strconv.Atoi(rawPort)
-	if err != nil || port < 0 || port > math.MaxUint16 {
-		return address, fmt.Errorf("invalid port: %s", rawPort)
-	}
-
-	return Address{
-		Host: host,
-		Port: uint16(port),
-	}, nil
-}
-
-func (a Address) SetPort(newPort uint16) Address {
-	a.Port = newPort
-
-	return a
-}
-
-func (a Address) IsLocalhost() bool {
-	if strings.HasPrefix(a.Host, "127.0.") {
-		return true
-	}
-
-	switch a.Host {
-	case "localhost", "0.0.0.0", "::1":
-		return true
-	}
-
-	return false
-}
-
-func (a Address) String() string {
-	return fmt.Sprintf("%s:%d", a.Host, a.Port)
+	return addr
 }
