439fa9fba141589541a882749aa48e6f28911fa9
fakefloordiv <real.floordiv@gmail.com>
2024-02-14 23:40:00 +0100

    added tests for new virtual router

---
+++ router/virtual/internal/domain/normalize_test.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNormalize(t *testing.T) {
+	t.Run("pure domain", func(t *testing.T) {
+		require.Equal(t, "foo.example.com", Normalize("foo.example.com"))
+	})
+
+	t.Run("with default port", func(t *testing.T) {
+		require.Equal(t, "foo.example.com", Normalize("foo.example.com:80"))
+		require.Equal(t, "foo.example.com", Normalize("foo.example.com:443"))
+	})
+
+	t.Run("with different port", func(t *testing.T) {
+		require.Equal(t, "foo.example.com:8080", Normalize("foo.example.com:8080"))
+	})
+
+	t.Run("with www prefix", func(t *testing.T) {
+		require.Equal(t, "foo.example.com", Normalize("www.foo.example.com"))
+	})
+
+	t.Run("ip address", func(t *testing.T) {
+		require.Equal(t, "1.1.1.1", Normalize("1.1.1.1:80"))
+	})
+}
+
+func TestTrimPort(t *testing.T) {
+	t.Run("with port", func(t *testing.T) {
+		require.Equal(t, "localhost", TrimPort("localhost:8080"))
+	})
+
+	t.Run("without port", func(t *testing.T) {
+		require.Equal(t, "localhost", TrimPort("localhost"))
+	})
+}
---
+++ router/virtual/virtual_test.go
@@ -0,0 +1,64 @@
+package virtual
+
+import (
+	"github.com/indigo-web/indigo/http"
+	"github.com/indigo-web/indigo/http/status"
+	"github.com/indigo-web/indigo/internal/initialize"
+	"github.com/indigo-web/indigo/internal/server/tcp/dummy"
+	"github.com/indigo-web/indigo/router/inbuilt"
+	"github.com/indigo-web/indigo/settings"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func newRequest(hosts ...string) *http.Request {
+	request := initialize.NewRequest(settings.Default(), dummy.NewNopConn(), nil)
+	request.Headers.Add("Host", hosts...)
+
+	return request
+}
+
+func TestVirtualRouter(t *testing.T) {
+	// as 404 Not Found can be returned only from an actual router, the value is
+	// considered to be positive
+	const OK = status.NotFound
+
+	t.Run("no hosts", func(t *testing.T) {
+		r := New()
+		require.NoError(t, r.OnStart())
+		require.True(t, requestIs(r.OnRequest(newRequest("localhost")), status.MisdirectedRequest))
+	})
+
+	t.Run("default router", func(t *testing.T) {
+		{
+			r := New().
+				Default(inbuilt.New())
+
+			require.NoError(t, r.OnStart())
+			require.True(t, requestIs(r.OnRequest(newRequest("localhost")), OK))
+			require.True(t, requestIs(r.OnRequest(newRequest("127.0.0.1")), OK))
+		}
+		{
+			r := New().
+				Host("0.0.0.0", inbuilt.New())
+
+			require.NoError(t, r.OnStart())
+			require.True(t, requestIs(r.OnRequest(newRequest("localhost")), OK))
+			require.True(t, requestIs(r.OnRequest(newRequest("127.0.0.1")), OK))
+		}
+	})
+
+	t.Run("single host", func(t *testing.T) {
+		r := New().
+			Host("pavlo.gay", inbuilt.New())
+
+		require.NoError(t, r.OnStart())
+		require.True(t, requestIs(r.OnRequest(newRequest("pavlo.gay")), OK))
+		require.True(t, requestIs(r.OnRequest(newRequest("localhost")), status.MisdirectedRequest))
+		require.True(t, requestIs(r.OnRequest(newRequest("pavlo.gay", "localhost")), status.BadRequest))
+	})
+}
+
+func requestIs(resp *http.Response, code status.Code) bool {
+	return resp.Reveal().Code == code
+}
