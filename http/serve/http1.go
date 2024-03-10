605fc9955f618945fbb5a6993b1e27b32900d773
fakefloordiv <real.floordiv@gmail.com>
2024-03-10 23:22:11 +0100

    rename core to simply serve

---
+++ http/serve/http1.go
@@ -0,0 +1,21 @@
+package serve
+
+import (
+	"github.com/indigo-web/indigo/config"
+	"github.com/indigo-web/indigo/http/encryption"
+	"github.com/indigo-web/indigo/internal/construct"
+	"github.com/indigo-web/indigo/internal/protocol/http1"
+	"github.com/indigo-web/indigo/router"
+	"net"
+)
+
+// HTTP1 setups and serves an HTTP/1.1 server until it stops. Note, that the connection isn't
+// automatically closed on server stop
+func HTTP1(cfg config.Config, conn net.Conn, enc encryption.Token, r router.Router) {
+	client := construct.Client(cfg.TCP, conn)
+	body := http1.NewBody(client, construct.Chunked(cfg.Body), cfg.Body)
+	request := construct.Request(cfg, client, body)
+	request.Env.Encryption = enc
+	suit := http1.Initialize(cfg, r, client, request)
+	suit.Serve()
+}
