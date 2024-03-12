0aba3d81c7b834a32540765c6fd61a8fbc630fff
fakefloordiv <real.floordiv@gmail.com>
2024-03-12 06:54:29 +0100

    minor code simplifying

--- router/inbuilt/aliases.go
+++ router/inbuilt/aliases.go
@@ -9,6 +9,5 @@ import (
 // before a handler is called. In case of implicit redirect, original path is stored in
 // Request.Env.AliasFrom
 func (r *Router) Alias(from, to string) *Router {
-	r.Mutator(mutator.Alias(path.Join(r.prefix, from), to))
-	return r
+	return r.Mutator(mutator.Alias(path.Join(r.prefix, from), to))
 }
