dce633e8f6ea295a40454ca3bacd12203f13a301
Pavlo <real.floordiv@gmail.com>
2024-10-11 19:43:57 +0200

    moved CutParams and CutHeader functions into the internal/strutil

--- internal/strutil/helpers.go
+++ internal/strutil/helpers.go
@@ -1,5 +1,7 @@
 package strutil
 
+import "strings"
+
 func LStripWS(str string) string {
 	for i, c := range str {
 		switch c {
@@ -23,6 +25,22 @@ func RStripWS(str string) string {
 	}
 
 	return ""
+}
+
+// CutParams behaves exactly as strings.Cut, but strips whitespaces between value
+// and the first-encountered parameter in addition.
+func CutParams(header string) (params string) {
+	_, params = CutHeader(header)
+	return params
+}
+
+func CutHeader(header string) (value, params string) {
+	sep := strings.IndexByte(header, ';')
+	if sep == -1 {
+		return header, ""
+	}
+
+	return header[:sep], LStripWS(header[sep+1:])
 }
 
 func Unquote(str string) string {
