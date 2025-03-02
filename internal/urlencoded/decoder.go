7f1321d2eab1b9110b38e7aa7226942cef06fe5a
Pavlo <real.floordiv@gmail.com>
2025-03-02 13:38:19 +0100

    made 2 separate decoders, both now have destination argument
    there will be probably some day an efficient implementation of decoding data strictly into "itself", however currently it's not a subject of work.

--- internal/urlencoded/decoder.go
+++ internal/urlencoded/decoder.go
@@ -7,53 +7,74 @@ import (
 	"github.com/indigo-web/utils/uf"
 )
 
-// Decode replaces all urlencoded sequences by corresponding ASCII characters into itself.
-func Decode(data []byte) ([]byte, error) {
-	for i := bytes.IndexByte(data, '%'); i != -1; i = bytes.IndexByte(data, '%') {
-		if i >= len(data)-2 {
-			return nil, status.ErrURLDecoding
-		}
-
-		a, b := hexconv.Halfbyte[data[i+1]], hexconv.Halfbyte[data[i+2]]
-		if a|b > 0x0f {
-			return nil, status.ErrURLDecoding
-		}
-		data[i] = (a << 4) | b
-		copy(data[i+1:], data[i+3:])
-		data = data[:len(data)-2]
-	}
-
-	return data, nil
-}
-
-// LazyDecode decodes data into the buffer on demand
-func LazyDecode(data []byte, buff []byte) (decoded []byte, buffer []byte, err error) {
-	percent := bytes.IndexByte(data, '%')
+// Decode decodes data into the given buffer, but omits it if there's no data to be
+// decoded. `into` can be data[:0] as well in order to decode "into itself".
+func Decode(src, dst []byte) (decoded, buffer []byte, err error) {
+	percent := bytes.IndexByte(src, '%')
 	if percent == -1 {
-		return data, buff, nil
+		return src, dst, nil
 	}
 
 	for percent != -1 {
-		if percent >= len(data)-2 {
-			return nil, buff, status.ErrURLDecoding
+		if percent >= len(src)-2 {
+			return nil, dst, status.ErrURLDecoding
 		}
 
-		buff = append(buff, data[:percent]...)
-		a, b := hexconv.Halfbyte[data[percent+1]], hexconv.Halfbyte[data[percent+2]]
+		dst = append(dst, src[:percent]...)
+		a, b := hexconv.Halfbyte[src[percent+1]], hexconv.Halfbyte[src[percent+2]]
 		if a|b > 0x0f {
-			return nil, buff, status.ErrURLDecoding
+			return nil, dst, status.ErrURLDecoding
 		}
 
-		buff = append(buff, (a<<4)|b)
-		data = data[percent+3:]
-		percent = bytes.IndexByte(data, '%')
+		dst = append(dst, (a<<4)|b)
+		src = src[percent+3:]
+		percent = bytes.IndexByte(src, '%')
 	}
 
-	buff = append(buff, data...)
-	return buff, buff, nil
+	dst = append(dst, src...)
+	return dst, dst, nil
 }
 
-func LazyDecodeString(data string, buff []byte) (string, []byte, error) {
-	d, buff, err := LazyDecode(uf.S2B(data), buff)
-	return uf.B2S(d), buff, err
+// ExtendedDecode is the same as Decode, but on top also decodes + as spaces.
+func ExtendedDecode(src, dst []byte) (decoded, buffer []byte, err error) {
+	dsthead := len(dst)
+	modified := false
+
+loop:
+	for i, c := range src {
+		if c == '+' {
+			modified = true
+			dst = append(dst, src[:i]...)
+			dst = append(dst, ' ')
+			src = src[i+1:]
+			goto loop
+		} else if c == '%' {
+			modified = true
+
+			if len(src)-i < 3 {
+				return nil, dst, status.ErrURLDecoding
+			}
+
+			a, b := hexconv.Halfbyte[src[i+1]], hexconv.Halfbyte[src[i+2]]
+			if a|b > 0x0f {
+				return nil, dst, status.ErrURLDecoding
+			}
+			dst = append(dst, src[:i]...)
+			dst = append(dst, (a<<4)|b)
+			src = src[i+3:]
+			goto loop
+		}
+	}
+
+	if !modified {
+		return src, dst, nil
+	}
+
+	dst = append(dst, src...)
+	return dst[dsthead:], dst, nil
+}
+
+func ExtendedDecodeString(src string, buff []byte) (decoded string, buffer []byte, err error) {
+	d, buffer, err := ExtendedDecode(uf.S2B(src), buff)
+	return uf.B2S(d), buffer, err
 }
--- internal/urlencoded/decoder_test.go
+++ internal/urlencoded/decoder_test.go
@@ -1,98 +1,151 @@
 package urlencoded
 
 import (
+	"fmt"
 	"github.com/indigo-web/indigo/http/status"
+	"github.com/indigo-web/utils/uf"
 	"github.com/stretchr/testify/require"
+	"math/rand/v2"
 	"strings"
 	"testing"
 )
 
-func testDecode(t *testing.T, decode func([]byte) ([]byte, error)) {
+func testDecoder(t *testing.T, decoder func([]byte, []byte) ([]byte, []byte, error)) {
 	t.Run("no escaping", func(t *testing.T) {
 		str := []byte("/hello")
-		decoded, err := Decode(str)
+		decoded, _, err := decoder(str, []byte{})
 		require.NoError(t, err)
 		require.Equal(t, "/hello", string(decoded))
 	})
 
 	t.Run("corners", func(t *testing.T) {
 		str := []byte("%2fhello%2f")
-		decoded, err := Decode(str)
+		decoded, _, err := decoder(str, []byte{})
 		require.NoError(t, err)
 		require.Equal(t, "/hello/", string(decoded))
 	})
 
 	t.Run("multiple consecutive", func(t *testing.T) {
 		str := []byte("%2f%20hello")
-		decoded, err := Decode(str)
+		decoded, _, err := decoder(str, []byte{})
 		require.NoError(t, err)
 		require.Equal(t, "/ hello", string(decoded))
 	})
 
 	t.Run("incomplete sequence", func(t *testing.T) {
 		str := []byte("%2")
-		_, err := Decode(str)
+		_, _, err := decoder(str, []byte{})
 		require.EqualError(t, err, status.ErrURLDecoding.Error())
 	})
 
 	t.Run("invalid code", func(t *testing.T) {
 		str := []byte("%2j")
-		_, err := Decode(str)
+		_, _, err := decoder(str, []byte{})
 		require.EqualError(t, err, status.ErrURLDecoding.Error())
 	})
 
 	t.Run("4kb slightly escaped", func(t *testing.T) {
 		str := []byte("/" + disperse("%5f", "a", 10, 4095))
-		decoded, err := Decode(str)
+		decoded, _, err := decoder(str, []byte{})
 		require.NoError(t, err)
 		want := "/" + strings.Repeat("_"+strings.Repeat("a", 10), 4095/len("%5f"+strings.Repeat("a", 10)))
 		require.Equal(t, want, string(decoded))
 		require.Equal(t, 4096, cap(decoded))
 	})
+
+	t.Run("decode into itself", func(t *testing.T) {
+		for _, tc := range []struct {
+			Encoded []byte
+			Want    string
+		}{
+			{[]byte("%2a"), "*"},
+			{[]byte("he%6c%6Co"), "hello"},
+			{[]byte("nothing here"), "nothing here"},
+		} {
+			decoded, _, err := decoder(tc.Encoded, tc.Encoded[:0])
+			require.NoError(t, err)
+			require.Equal(t, tc.Want, string(decoded))
+		}
+	})
 }
 
 func TestDecode(t *testing.T) {
-	testDecode(t, Decode)
+	testDecoder(t, Decode)
 }
 
-func TestLazyDecode(t *testing.T) {
-	testDecode(t, func(bytes []byte) ([]byte, error) {
-		data, _, err := LazyDecode(bytes, nil)
-		return data, err
-	})
+func TestExtendedDecode(t *testing.T) {
+	testDecoder(t, ExtendedDecode)
+}
+
+func bench(b *testing.B, name string, decoder func(src, dst []byte) (decoded, buff []byte, err error)) {
+	sizes := []int{
+		4096,  /*4kb*/
+		32768, /*32kb*/
+		65536, /*64kb*/
+	}
+	buff := make([]byte, 0, sizes[len(sizes)-1])
+	proportions := []struct{ A, B int }{
+		{0, 1},
+		{1, 5},
+		{3, 5},
+		{1, 1},
+		{1, 0},
+	}
+
+	for _, size := range sizes {
+		for _, prop := range proportions {
+			b.Run(
+				fmt.Sprintf("%s %d bytes %d:%d proportion", name, size, prop.A, prop.B),
+				func(b *testing.B) {
+					str := uf.S2B(mix("%2a", "a", prop.A, prop.B, size))
+					b.ReportAllocs()
+					b.SetBytes(int64(len(str)))
+					b.ResetTimer()
+
+					for i := 0; i < b.N; i++ {
+						_, _, _ = decoder(str, buff)
+					}
+				},
+			)
+		}
+	}
 }
 
 func BenchmarkDecode(b *testing.B) {
-	bench := func(b *testing.B, segment string) {
-		str := []byte("/" + strings.Repeat(segment, 4095/len(segment)))
-		b.SetBytes(int64(len(str)))
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			_, _ = Decode(str)
-		}
-	}
-
-	b.Run("4kb unescaped", func(b *testing.B) {
-		bench(b, "a")
-	})
-
-	b.Run("4kb slightly escaped", func(b *testing.B) {
-		// one urlencoded part per 10 decoded characters
-		bench(b, "%5faaaaaaaaa")
-	})
-
-	b.Run("4kb half escaped", func(b *testing.B) {
-		bench(b, "%5fa")
-	})
-
-	b.Run("4kb only escaped", func(b *testing.B) {
-		bench(b, "%5f")
-	})
+	bench(b, "Decode", Decode)
 }
 
-// disperse makes a string, which consists of 1:proportion substrings a and b respectfully.
-// Repeating them doesn't always result in exactly desiredLen bytes
-func disperse(a, b string, proportion, desiredLen int) string {
-	return strings.Repeat(a+strings.Repeat(b, proportion), desiredLen/(len(a)+len(b)*proportion))
+func BenchmarkExtendedDecode(b *testing.B) {
+	bench(b, "ExtendedDecode", ExtendedDecode)
+}
+
+// mix produces a mix of a and b substrings, randomly distributed in respect to given
+// proportions over the (not necessarily exactly) `length` bytes
+func mix(a, b string, propA, propB, length int) string {
+	ratio := length / (len(a)*propA + len(b)*propB)
+	as, bs := propA*ratio, propB*ratio
+	arr := make([]string, 0, as+bs)
+
+	for range as {
+		arr = append(arr, a)
+	}
+	for range bs {
+		arr = append(arr, b)
+	}
+
+	rand.Shuffle(len(arr), func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	return strings.Join(arr, "")
+}
+
+// disperse returns a string of length `length` or more. Guaranteed to not break the sequences.
+// The resulting string consists of substrings `a` and `b`, where the proportion of `a:b` is equal
+// to `1:proportion`. Produces a predictable string, which is desired in tests.
+func disperse(a, b string, proportion, length int) string {
+	return strings.Repeat(
+		a+strings.Repeat(b, proportion),
+		length/(len(a)+len(b)*proportion),
+	)
 }
