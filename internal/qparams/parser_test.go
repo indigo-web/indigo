8df050b05fe280af48acc1c236ae89db89d9a718
Pavlo <real.floordiv@gmail.com>
2025-03-02 13:33:00 +0100

    nonprintable ASCII validation, updated decoder contract, destination argument and overriding default flag value

--- internal/qparams/parser_test.go
+++ internal/qparams/parser_test.go
@@ -10,10 +10,12 @@ import (
 )
 
 func TestParamsParser(t *testing.T) {
+	const defFlagVal = "1"
+
 	t.Run("single pair", func(t *testing.T) {
 		query := "hello=world"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hello"))
 		require.Equal(t, "world", result.Value("hello"))
@@ -22,7 +24,7 @@ func TestParamsParser(t *testing.T) {
 	t.Run("two pairs", func(t *testing.T) {
 		query := "hello=world&lorem=ipsum"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hello"))
 		require.Equal(t, "world", result.Value("hello"))
@@ -33,7 +35,7 @@ func TestParamsParser(t *testing.T) {
 	t.Run("empty value before ampersand", func(t *testing.T) {
 		query := "hello=&another=pair"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hello"))
 		require.Empty(t, result.Value("hello"))
@@ -42,7 +44,7 @@ func TestParamsParser(t *testing.T) {
 	t.Run("single entry without value", func(t *testing.T) {
 		query := "hello="
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hello"))
 		require.Empty(t, result.Value("hello"))
@@ -50,14 +52,14 @@ func TestParamsParser(t *testing.T) {
 
 	t.Run("empty key", func(t *testing.T) {
 		query := "=world"
-		err := Parse([]byte(query), Into(keyvalue.New()), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(keyvalue.New()), urlencoded.Decode, defFlagVal)
 		require.ErrorIs(t, err, status.ErrBadRequest)
 	})
 
 	t.Run("ampersand without continuation at the end", func(t *testing.T) {
 		query := "hello=world&"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hello"))
 		require.Equal(t, "world", result.Value("hello"))
@@ -66,35 +68,35 @@ func TestParamsParser(t *testing.T) {
 	t.Run("flag", func(t *testing.T) {
 		result := keyvalue.New()
 
-		for _, paramsString := range []string{
+		for _, str := range []string{
 			"lorem&hello=world&foo=bar",
 			"hello=world&lorem&foo=bar",
 			"hello=world&foo=bar&lorem",
 		} {
-			err := Parse([]byte(paramsString), Into(result), urlencoded.Decode)
-			require.NoError(t, err, paramsString)
-			require.True(t, result.Has("hello"), paramsString)
-			require.Equal(t, "world", result.Value("hello"), paramsString)
-			require.True(t, result.Has("foo"), paramsString)
-			require.Equal(t, "bar", result.Value("foo"), paramsString)
-			require.True(t, result.Has("lorem"), paramsString)
-			require.Equal(t, defaultEmptyValueContent, result.Value("lorem"), paramsString)
+			_, err := Parse([]byte(str), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
+			require.NoError(t, err, str)
+			require.True(t, result.Has("hello"), str)
+			require.Equal(t, "world", result.Value("hello"), str)
+			require.True(t, result.Has("foo"), str)
+			require.Equal(t, "bar", result.Value("foo"), str)
+			require.True(t, result.Has("lorem"), str)
+			require.Equal(t, defFlagVal, result.Value("lorem"), str)
 		}
 	})
 
 	t.Run("single flag", func(t *testing.T) {
 		query := "lorem"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.Decode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("lorem"))
-		require.Equal(t, defaultEmptyValueContent, result.Value("lorem"))
+		require.Equal(t, defFlagVal, result.Value("lorem"))
 	})
 
 	t.Run("encoded spaces", func(t *testing.T) {
 		query := "hel+lo=wo+rld"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.ExtendedDecode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hel lo"))
 		require.Equal(t, "wo rld", result.Value("hel lo"))
@@ -103,7 +105,7 @@ func TestParamsParser(t *testing.T) {
 	t.Run("url encoded", func(t *testing.T) {
 		query := "hel%20lo=wo%20rld%21"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.ExtendedDecode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hel lo"))
 		require.Equal(t, "wo rld!", result.Value("hel lo"))
@@ -112,7 +114,7 @@ func TestParamsParser(t *testing.T) {
 	t.Run("encoded plus char", func(t *testing.T) {
 		query := "hel%2blo=wo%2brld"
 		result := keyvalue.New()
-		err := Parse([]byte(query), Into(result), urlencoded.Decode)
+		_, err := Parse([]byte(query), []byte{}, Into(result), urlencoded.ExtendedDecode, defFlagVal)
 		require.NoError(t, err)
 		require.True(t, result.Has("hel+lo"))
 		require.Equal(t, "wo+rld", result.Value("hel+lo"))
