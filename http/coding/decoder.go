d65283d5ebc6508918a4c0a1d6faa863c5181503
Pavlo <real.floordiv@gmail.com>
2025-03-03 11:53:56 +0100

    temporary drafts

---
+++ http/coding/decoder.go
@@ -0,0 +1,4 @@
+package coding
+
+type Decoder struct {
+}
---
+++ http/codings.go
@@ -0,0 +1,11 @@
+package http
+
+//type decoder interface {
+//	Retriever
+//	Reset(inner Retriever)
+//}
+//
+//type decoder struct {
+//	retriever Retriever
+//	decoders  []decoder
+//}
