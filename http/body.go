c5833b06d89f8152ff872250c8b6a833f1dbf450
Pavlo <pavlo.romanenko@edu.ruhr-uni-bochum.de>
2025-06-14 23:22:41 +0200

    renamed Retriever interface, decoupled the body from the request entity
    the Retriever interface is now Fetcher with Fetch() method instead of previous Retrieve(). The reason is that "to fetch" sounds more naturally and better-suited for the usecase.

--- http/body.go
+++ http/body.go
@@ -1,40 +1,46 @@
 package http
 
 import (
+	"github.com/flrdv/uf"
 	"github.com/indigo-web/indigo/config"
 	"github.com/indigo-web/indigo/http/form"
 	"github.com/indigo-web/indigo/http/mime"
 	"github.com/indigo-web/indigo/http/status"
 	"github.com/indigo-web/indigo/internal/formdata"
 	"github.com/indigo-web/indigo/internal/strutil"
-	"github.com/indigo-web/utils/uf"
 	json "github.com/json-iterator/go"
 	"io"
 )
 
 type BodyCallback func([]byte) error
 
-type Retriever interface {
-	// Retrieve reads and returns a piece of body available for processing
-	Retrieve() ([]byte, error)
+// Fetcher abstracts the underlying protocol-dependant body source. Even though the signature
+// is identical to transport.Client.Read(), it is named differently in order to highlight the
+// difference between relatively low-level connection "raw data" and high-level body
+// data streams.
+type Fetcher interface {
+	Fetch() ([]byte, error)
 }
 
 type Body struct {
-	Retriever
-	request  *Request
-	cfg      *config.Config
-	form     form.Form
-	buff     []byte
-	formbuff []byte
-	pending  []byte
-	error    error
+	Fetcher
+
+	cfg         *config.Config
+	contentType string
+	buff        []byte
+	formbuff    []byte
+	pending     []byte
+	form        form.Form
+	error       error
 }
 
-func NewBody(r *Request, src Retriever, cfg *config.Config) *Body {
+// TODO: body entity can be passed by value, as it is anyway going to be stored in the request entity,
+// TODO: which is in turn is already on heap.
+
+func NewBody(cfg *config.Config, src Fetcher) *Body {
 	return &Body{
-		Retriever: src,
-		request:   r,
-		cfg:       cfg,
+		Fetcher: src,
+		cfg:     cfg,
 	}
 }
 
@@ -51,7 +57,7 @@ func (b *Body) Callback(cb BodyCallback) error {
 
 	for {
 		var data []byte
-		data, b.error = b.Retrieve()
+		data, b.error = b.Fetch()
 		switch b.error {
 		case nil:
 		case io.EOF:
@@ -82,7 +88,7 @@ func (b *Body) Bytes() ([]byte, error) {
 
 	for {
 		var data []byte
-		data, b.error = b.Retrieve()
+		data, b.error = b.Fetch()
 		b.buff = append(b.buff, data...)
 		switch b.error {
 		case nil:
@@ -103,7 +109,7 @@ func (b *Body) String() (string, error) {
 // Read implements the io.Reader interface.
 func (b *Body) Read(into []byte) (n int, err error) {
 	if len(b.pending) == 0 && b.error == nil {
-		b.pending, b.error = b.Retrieve()
+		b.pending, b.error = b.Fetch()
 	}
 
 	n = copy(into, b.pending)
@@ -124,7 +130,7 @@ func (b *Body) Read(into []byte) (n int, err error) {
 //
 // TODO: make possible to choose and use different from json-iterator json marshall/unmarshall
 func (b *Body) JSON(model any) error {
-	if !mime.Complies(mime.JSON, b.request.ContentType) {
+	if !mime.Complies(mime.JSON, b.contentType) {
 		return status.ErrUnsupportedMediaType
 	}
 
@@ -160,10 +166,10 @@ func (b *Body) Form() (f form.Form, err error) {
 	}
 
 	switch {
-	case mime.Complies(mime.FormUrlencoded, b.request.ContentType):
-		f, b.formbuff, err = formdata.ParseURLEncoded(b.form[:0], raw, b.formbuff[:0], b.cfg.URL.Query.DefaultFlagValue)
+	case mime.Complies(mime.FormUrlencoded, b.contentType):
+		f, b.formbuff, err = formdata.ParseURLEncoded(b.form[:0], raw, b.formbuff[:0])
 		return f, err
-	case mime.Complies(mime.Multipart, b.request.ContentType):
+	case mime.Complies(mime.Multipart, b.contentType):
 		boundary, ok := b.multipartBoundary()
 		if !ok {
 			return nil, status.ErrBadRequest
@@ -175,11 +181,10 @@ func (b *Body) Form() (f form.Form, err error) {
 	}
 }
 
-// Discard discards the rest of the body (if any). If no networking error was encountered,
-// nil is returned.
+// Discard sinkholes the rest of the body. Should not be used unless you know what you're doing.
 func (b *Body) Discard() error {
 	for b.error == nil {
-		_, b.error = b.Retrieve()
+		_, b.error = b.Fetch()
 	}
 
 	if b.error == io.EOF {
@@ -194,18 +199,15 @@ func (b *Body) Error() error {
 	return b.error
 }
 
-func (b *Body) Reset() error {
-	if err := b.Discard(); err != nil {
-		return err
-	}
-
+// Reset resets the body state. Should never be used as serves internal purposes only.
+func (b *Body) Reset(request *Request) {
 	b.error = nil
 	b.buff = b.buff[:0]
-	return nil
+	b.contentType = request.ContentType
 }
 
 func (b *Body) multipartBoundary() (boundary string, ok bool) {
-	for key, value := range strutil.WalkKV(strutil.CutParams(b.request.ContentType)) {
+	for key, value := range strutil.WalkKV(strutil.CutParams(b.contentType)) {
 		if key == "boundary" {
 			if len(boundary) != 0 {
 				return "", false
