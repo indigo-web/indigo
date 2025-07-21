4f24e15b7f49a23f2ae73587ac3b8bd198a38e0c
Pavlo <pavlo.romanenko@edu.ruhr-uni-bochum.de>
2025-07-21 22:03:21 +0200

    added a lazy variant of the strings.Join exploiting new iterators

---
+++ internal/strutil/join_test.go
@@ -0,0 +1,29 @@
+package strutil
+
+import (
+	"github.com/stretchr/testify/require"
+	"iter"
+	"testing"
+)
+
+func asIterator(elems ...string) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		for _, elem := range elems {
+			yield(elem)
+		}
+	}
+}
+
+func TestJoin(t *testing.T) {
+	str := Join(asIterator(), ", ")
+	require.Empty(t, str)
+
+	str = Join(asIterator("hello"), ", ")
+	require.Equal(t, "hello", str)
+
+	str = Join(asIterator("hello", "world"), ", ")
+	require.Equal(t, "hello, world", str)
+
+	str = Join(asIterator("hello", "world", "as usual"), ", ")
+	require.Equal(t, "hello, world, as usual", str)
+}
