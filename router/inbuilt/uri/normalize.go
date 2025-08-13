fe7d972260cd241250763f22eecb399afdc46176
Pavlo <pavlo.romanenko@edu.ruhr-uni-bochum.de>
2025-08-13 02:28:37 +0200

    made in path normalization deleting just single trailing slash

--- router/inbuilt/uri/normalize.go
+++ router/inbuilt/uri/normalize.go
@@ -3,10 +3,8 @@ package uri
 // Normalize removes trailing slashes, as all request paths are also trimmed, resulting
 // in consensus between these two.
 func Normalize(path string) string {
-	for i := len(path) - 1; i > 1; i-- {
-		if path[i] != '/' {
-			return path[:i+1]
-		}
+	if len(path) > 1 && path[len(path)-1] == '/' {
+		return path[:len(path)-1]
 	}
 
 	return path
