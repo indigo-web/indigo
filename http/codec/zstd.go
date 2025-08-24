7b28e2a0ebdb9f778f426d936c3cdaefee833e9b
Pavlo <pavlo.romanenko@edu.ruhr-uni-bochum.de>
2025-08-24 16:39:49 +0200

    added deflate and zstd codecs

---
+++ http/codec/zstd.go
@@ -0,0 +1,21 @@
+package codec
+
+import (
+	"github.com/klauspost/compress/zstd"
+)
+
+func NewZSTD() Codec {
+	w, err := zstd.NewWriter(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := zstd.NewReader(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	instantiator := newBaseInstance(w, r, genericResetter)
+
+	return newBaseCodec("zstd", gzipBufferSize, instantiator)
+}
